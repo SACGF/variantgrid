868cf9a1eb92d286d61f4e1aa7338b4bff0172b0
Dave Lawrence <davmlaw@gmail.com>
2020-10-11 22:09:04 +1030

    Found missing when installing from scratch

--- scripts/install/ubuntu_20_install_dependencies.sh
+++ scripts/install/ubuntu_20_install_dependencies.sh
@@ -4,9 +4,9 @@ set -e # Fail on error
 
 echo "Installing packages via apt-get"
 apt-get update
-apt-get install -y git-core python3-dev python3-pip gfortran libopenblas-dev liblapack-dev zlib1g-dev bedtools libbz2-dev liblzma-dev libssl-dev libcurl4-openssl-dev curl
+apt-get install -y git-core python3-dev python3-pip python3-gdal gfortran libopenblas-dev liblapack-dev zlib1g-dev bedtools libbz2-dev liblzma-dev libssl-dev libcurl4-openssl-dev curl
 apt-get install -y libblas-dev libatlas-base-dev libpng-dev libjpeg-dev libfreetype6-dev libpq-dev libxft-dev zlib1g-dev libxml2-dev libxslt1-dev rustc
-apt-get install -y nginx redis-server rabbitmq-server postgresql postgresql-contrib libpq-dev
+apt-get install -y gunicorn nginx redis-server rabbitmq-server postgresql postgresql-contrib libpq-dev
 # actually only needed for development
 apt-get install -y sassc
 
--- variantgrid/settings/env/_settings_template.py
+++ variantgrid/settings/env/_settings_template.py
@@ -7,7 +7,7 @@ from variantgrid.settings.components.seqauto_settings import *  # pylint: disabl
 WEB_HOSTNAME = 'yourdomain.com'
 WEB_IP = '127.0.0.1'
 
-ALLOWED_HOSTS = [WEB_HOSTNAME, WEB_IP]
+ALLOWED_HOSTS = ["localhost", WEB_HOSTNAME, WEB_IP]
 
 # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
 
