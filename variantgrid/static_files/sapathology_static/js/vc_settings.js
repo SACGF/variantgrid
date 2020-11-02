19f468c81fdf9297dae9a4c7a739be4fae2b3393
David Lawrence <davmlaw@gmail.com>
2020-11-02 11:40:14 +1030

    Move SA Path static and templates out of core

--- variantgrid/static_files/sapathology_static/js/vc_settings.js
+++
@@ -1,7 +0,0 @@
-let VcSettings = {};
-VcSettings.LAB = 'Lab';
-VcSettings.INSTITUTION = 'Institution';
-VcSettings.LOGGED_IN_USERS = 'Shariant Users';
-VcSettings.PUBLIC = '3rd Party Databases';
-
-VcSettings.SHARE_LEVELS = ['lab', 'institution', 'logged_in_users', 'public'];
