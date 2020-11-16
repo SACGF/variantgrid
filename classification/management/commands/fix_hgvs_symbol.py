b1a3a9dcd80eec3809edb1cb02129a2bca12f14a
TheMadBug <jimmy.andrews@gmail.com>
2020-11-16 13:16:04 +1030

    Run update cached c hgvs after updating c.hgvs to catch any mismatch

--- classification/management/commands/fix_hgvs_symbol.py
+++ classification/management/commands/fix_hgvs_symbol.py
@@ -26,25 +26,26 @@ class Command(BaseCommand):
                 evidence[SpecialEKeys.C_HGVS]["value"] = desired_chgvs
                 if not test:
                     c.save(update_fields=["evidence"])
+                    c.update_cached_c_hgvs()
 
                 changed_vcs = 0
-                vc: ClassificationModification
-                for vc in ClassificationModification.objects.filter(classification_id=c_id):
+                vm: ClassificationModification
+                for vm in ClassificationModification.objects.filter(classification_id=c_id):
                     evidence_changed = False
-                    if vc_evidence := vc.published_evidence:
+                    if vc_evidence := vm.published_evidence:
                         if SpecialEKeys.C_HGVS in vc_evidence:
                             if vc_evidence[SpecialEKeys.C_HGVS].get("value") == c_hgvs:
                                 vc_evidence[SpecialEKeys.C_HGVS]["value"] = desired_chgvs
                                 evidence_changed = True
 
                     delta_changed = False
-                    if vc_delta := vc.delta:
+                    if vc_delta := vm.delta:
                         if SpecialEKeys.C_HGVS in vc_delta:
                             if vc_delta[SpecialEKeys.C_HGVS].get("value") == c_hgvs:
                                 vc_delta[SpecialEKeys.C_HGVS]["value"] = desired_chgvs
                                 delta_changed = True
 
                     if not test:
-                        vc.save(update_fields=["published_evidence", "delta"])
+                        vm.save(update_fields=["published_evidence", "delta"])
 
-                print(f"Classification classification {c_id} {c_hgvs} - > {desired_chgvs}")
+                print(f"Classification classification {c_id} {c_hgvs} -> {desired_chgvs}")
