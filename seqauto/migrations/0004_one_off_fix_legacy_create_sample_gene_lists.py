a441240ef37ee27f2989d86052d6d8a690fa84e8
David Lawrence <davmlaw@gmail.com>
2020-12-09 14:57:10 +1030

    Handle dodgy QC Gene lists

--- seqauto/migrations/0004_one_off_fix_legacy_create_sample_gene_lists.py
+++ seqauto/migrations/0004_one_off_fix_legacy_create_sample_gene_lists.py
@@ -45,10 +45,13 @@ def _fix_seqauto_qc_gene_list(apps, schema_editor):
                 try:
                     custom_gene_list_text = f.read()
                     md5_hash = md5sum_str(custom_gene_list_text)
+                    file_unchanged = md5_hash == qcgl.custom_text_gene_list.md5_hash
                 except UnicodeDecodeError as e:
-                    print(f"Error reading file: {qcgl.path}: {e}")
-                    raise e
-                if md5_hash == qcgl.custom_text_gene_list.md5_hash:
+                    print(f"Skipping QC Gene List: '{qcgl.path}': {e}")
+                    num_skipped += 1
+                    continue
+
+                if file_unchanged:
                     num_set_to_latest += 1
                     qcgl.file_last_modified = path.stat().st_mtime
                     qc_gene_list_list.append(qcgl)
