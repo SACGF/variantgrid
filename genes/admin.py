4147860cd8513c888593f6dae9f6e0f08040e8b5
David Lawrence <davmlaw@gmail.com>
2021-01-15 10:57:24 +1030

    Sort and add panel app panel stuff to admin

--- genes/admin.py
+++ genes/admin.py
@@ -3,11 +3,13 @@ from guardian.admin import GuardedModelAdmin
 
 from genes import models
 
+admin.site.register(models.ActiveSampleGeneList)
 admin.site.register(models.GeneAnnotationRelease)
 admin.site.register(models.GeneInfo)
-admin.site.register(models.GeneListGeneSymbol)
 admin.site.register(models.GeneList, GuardedModelAdmin)
+admin.site.register(models.GeneListGeneSymbol)
 admin.site.register(models.GeneSymbol)
 admin.site.register(models.GeneSymbolAlias)
-admin.site.register(models.ActiveSampleGeneList)
+admin.site.register(models.PanelAppPanel)
+admin.site.register(models.PanelAppPanelLocalCacheGeneList)
 admin.site.register(models.SampleGeneList)
