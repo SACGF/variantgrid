b5a8f349c8b88d4ded1414d4183f7c05d4fb37d4
David Lawrence <davmlaw@gmail.com>
2021-02-25 16:49:59 +1030

    VCF header fix command (SA Path test)

---
+++ upload/management/commands/fix_incomplete_vcf_headers.py
@@ -0,0 +1,29 @@
+import os
+
+from cyvcf2 import Reader
+from django.core.management.base import BaseCommand
+from django.db.models import Q
+
+from snpdb.models import VCF
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        q_no_header = Q(header__isnull=True)
+        q_no_chrom_line = ~Q(header__icontains='#CHROM')
+
+        num_fixed = 0
+        missing_file = 0
+        for vcf in VCF.objects.filter(q_no_header | q_no_chrom_line):
+            filename = vcf.uploadedvcf.uploaded_file.get_filename()
+            if os.path.exists(filename):
+                reader = Reader(filename)
+                vcf.header = reader.raw_header
+                vcf.save()
+                num_fixed += 1
+                if num_fixed % 100 == 0:
+                    print(f"Fixed {num_fixed}")
+            else:
+                missing_file += 1
+
+        print(f"VCFs with incomplete headers - fixed: {num_fixed}, missing: {missing_file}")
