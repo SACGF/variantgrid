654f05190ac1535807b2e4a903fe3f7eb76a1b90
David Lawrence <davmlaw@gmail.com>
2021-02-25 17:02:02 +1030

    Fix VCF header

--- upload/management/commands/fix_incomplete_vcf_headers.py
+++ upload/management/commands/fix_incomplete_vcf_headers.py
@@ -5,6 +5,7 @@ from django.core.management.base import BaseCommand
 from django.db.models import Q
 
 from snpdb.models import VCF
+from upload.models import UploadedVCF
 
 
 class Command(BaseCommand):
@@ -15,15 +16,18 @@ class Command(BaseCommand):
         num_fixed = 0
         missing_file = 0
         for vcf in VCF.objects.filter(q_no_header | q_no_chrom_line):
-            filename = vcf.uploadedvcf.uploaded_file.get_filename()
-            if os.path.exists(filename):
-                reader = Reader(filename)
-                vcf.header = reader.raw_header
-                vcf.save()
-                num_fixed += 1
-                if num_fixed % 100 == 0:
-                    print(f"Fixed {num_fixed}")
-            else:
+            try:
+                filename = vcf.uploadedvcf.uploaded_file.get_filename()
+                if os.path.exists(filename):
+                    reader = Reader(filename)
+                    vcf.header = reader.raw_header
+                    vcf.save()
+                    num_fixed += 1
+                    if num_fixed % 100 == 0:
+                        print(f"Fixed {num_fixed}")
+                else:
+                    missing_file += 1
+            except UploadedVCF.DoesNotExist:
                 missing_file += 1
 
         print(f"VCFs with incomplete headers - fixed: {num_fixed}, missing: {missing_file}")
