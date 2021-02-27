062898bf05a56276feefba3717545e03bacb33f3
TheMadBug <jimmy.andrews@gmail.com>
2021-02-27 12:56:53 +1030

    Get rid of debugging in session refresh management

--- auth/session_refresh.py
+++ auth/session_refresh.py
@@ -5,7 +5,5 @@ class VariantGridSessionRefresh(SessionRefresh):
 
     def is_refreshable_url(self, request):
         if '/api/' in request.path or request.is_ajax():
-            print(f"{request.path} That looks like an API, aint going to refresh check")
             return False
-        print(f"{request.path} does not look like API")
         return super().is_refreshable_url(request)
