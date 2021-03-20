9d488a13ae0be6101d4a8ac33770549efe838fce
David Lawrence <davmlaw@gmail.com>
2021-03-20 21:55:00 +1030

    Annotation Runs - add some buttons that help manage runs

--- annotation/tasks/annotate_variants.py
+++ annotation/tasks/annotate_variants.py
@@ -140,6 +140,10 @@ def dump_and_annotate_variants(annotation_run):
 
 
 def annotation_run_retry(annotation_run: AnnotationRun, upload_only=False) -> AnnotationRun:
+    if annotation_run.status != AnnotationStatus.ERROR:
+        msg = f"Cannot re-try {annotation_run} as has non-error status {annotation_run.get_status_display()}"
+        raise ValueError(msg)
+
     if upload_only and annotation_run.vcf_annotated_filename is None:
         msg = "Retry annotation run upload only requires annotation VCF to be written"
         raise ValueError(msg)
