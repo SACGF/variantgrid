11e958f03a70c2f472436b74e8581df8cca6207e
David Lawrence <davmlaw@gmail.com>
2021-03-24 10:05:10 +1030

    Find new files

--- seqauto/scripts/tau/find_qc.sh
+++ seqauto/scripts/tau/find_qc.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-find /tau/data/clinical_hg38 -name "*.txt"
+# This needs to return all QC objects
+TAU_DATA_DIR=/tau/data/clinical_hg38
+
+find ${TAU_DATA_DIR} -name "*_stats.txt" -o -name "*_qc_summary.txt" -o -name "*.per_gene_coverage.tsv.gz"
