f16e8abe7bd606171ca94dce2192ffbbd50b1264
TheMadBug <jimmy.andrews@gmail.com>
2021-03-26 12:03:42 +1100

    Fix server status page

--- variantopedia/views.py
+++ variantopedia/views.py
@@ -95,7 +95,7 @@ def get_dashboard_notices(user: User, days_ago: Optional[int]) -> dict:
     classifications_of_interest = Classification.dashboard_report_classifications_of_interest(since=start_time)
     new_classification_count = Classification.dashboard_report_new_classifications(since=start_time)
 
-    any_notices = any(qs.exists() for qs in [events, vcfs, analyses_created, analyses_modified, classifications_of_interest, new_classification_count])
+    any_notices = any(qs.exists() for qs in [events, vcfs, analyses_created, analyses_modified]) or bool(classifications_of_interest) or bool(new_classification_count)
 
     dashboard_notices = {}
     if any_notices:
