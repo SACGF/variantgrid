990633a587ef9f7453d5a3571f7465c350f2ed2d
David Lawrence <davmlaw@gmail.com>
2021-04-29 16:11:09 +0930

    Only show warning if anything happened

--- variantopedia/search.py
+++ variantopedia/search.py
@@ -493,7 +493,8 @@ def search_hgvs(search_string: str, user: User, genome_build: GenomeBuild, varia
             try:
                 hgvs_string = HGVSMatcher.clean_hgvs(hgvs_string)
                 variant_tuple = hgvs_matcher.get_variant_tuple(hgvs_string)
-                search_messages.append(f"Warning: Cleaned '{search_string}' => '{hgvs_string}'")
+                if search_string != hgvs_string:
+                    search_messages.append(f"Warning: Cleaned '{search_string}' => '{hgvs_string}'")
             except InvalidHGVSName:
                 if classify:
                     search_message = f"Error reading HGVS: '{original_error}'"
