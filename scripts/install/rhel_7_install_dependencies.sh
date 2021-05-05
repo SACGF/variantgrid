8b97d2cb23bb48af0863f935a24c3fe791c5909d
Dave Lawrence <davmlaw@gmail.com>
2021-05-05 18:13:11 +0930

    Update rhel_7_install_dependencies.sh
    Add postgresql12-contrib - need for CIText extension for case insensitive fields

--- scripts/install/rhel_7_install_dependencies.sh
+++ scripts/install/rhel_7_install_dependencies.sh
@@ -11,7 +11,7 @@ yum install python3-devel libcurl-devel # Needed for pip packages - CyVCF2/pysam
 yum -y install https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
 yum -y install epel-release yum-utils
 yum-config-manager --enable pgdg12
-yum install postgresql12-server postgresql12
+yum install postgresql12-server postgresql12 postgresql12-contrib
 
 /usr/pgsql-12/bin/postgresql-12-setup initdb
 systemctl enable --now postgresql-12
