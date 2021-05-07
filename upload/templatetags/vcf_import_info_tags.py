e36c59eaf6b66b555218767235b6f15ada040518
Dave Lawrence <davmlaw@gmail.com>
2021-05-07 10:59:23 +0930

    variantgrid_sapath #125 - read only user sample page / GOI

--- upload/templatetags/vcf_import_info_tags.py
+++ upload/templatetags/vcf_import_info_tags.py
@@ -20,6 +20,8 @@ def vcf_import_info(vcf: VCF, examine_error_instructions):
         msg = 'Import Error: This file has no associated import information. Please re-upload.'
         errors.append(msg)
 
-    return {"errors": errors,
-            "warnings": warnings,
-            "examine_error_instructions": examine_error_instructions}
+    return {
+        "errors": errors,
+        "warnings": warnings,
+        "examine_error_instructions": examine_error_instructions
+    }
