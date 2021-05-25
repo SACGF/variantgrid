ce66a7f1716c85144177fcce89b895105b249779
David Lawrence <davmlaw@gmail.com>
2021-05-25 15:29:17 +0930

    Be able to do updates in small chunks

--- annotation/management/commands/fix_impact_change_choice.py
+++ annotation/management/commands/fix_impact_change_choice.py
@@ -7,23 +7,37 @@ from annotation.models import VariantAnnotationVersion, VariantAnnotation, Varia
 
 class Command(BaseCommand):
     """ Do this as a management command not migration so its not in a transaction (which got too big) """
+    def add_arguments(self, parser):
+        parser.add_argument('--small_updates', action='store_true', help="Do update in small chunks (to reduce transaction size)")
 
     def handle(self, *args, **options):
-        DamageNode.objects.update(impact_min=self._get_case("impact_min"))
-
-        impact_case = self._get_case("impact")
-        for vav in VariantAnnotationVersion.objects.all():
-            print(f"Updating {vav} VariantAnnotation... (may take a while)")
-            VariantAnnotation.objects.filter(version=vav).update(impact=impact_case)
-            print(f"Updating {vav} VariantTranscriptAnnotation... (may take a really long while)")
-            VariantTranscriptAnnotation.objects.filter(version=vav).update(impact=impact_case)
-
-    @staticmethod
-    def _get_case(field_name) -> Case:
+        small_updates = options["small_updates"]
         IMPACT_OLD_NEW = {
             "O": "1",
             "L": "2",
             "M": "3",
             "H": "4",
         }
-        return Case(*[When(**{field_name: k}, then=Value(v)) for k, v in IMPACT_OLD_NEW.items()])
+
+        DamageNode.objects.update(impact_min=self._get_case(IMPACT_OLD_NEW, "impact_min"))
+
+        if small_updates:
+            for vav in VariantAnnotationVersion.objects.all():
+                for old, new in IMPACT_OLD_NEW.items():
+                    print(f"Updating {vav} VariantAnnotation {old} -> {new}")
+                    VariantAnnotation.objects.filter(version=vav, impact=old).update(impact=new)
+                for old, new in IMPACT_OLD_NEW.items():
+                    print(f"Updating {vav} VariantTranscriptAnnotation... {old} -> {new}")
+                    VariantTranscriptAnnotation.objects.filter(version=vav, impact=old).update(impact=new)
+        else:
+            # Do as 1 big case statement
+            impact_case = self._get_case(IMPACT_OLD_NEW, "impact")
+            for vav in VariantAnnotationVersion.objects.all():
+                print(f"Updating {vav} VariantAnnotation... (may take a while)")
+                VariantAnnotation.objects.filter(version=vav).update(impact=impact_case)
+                print(f"Updating {vav} VariantTranscriptAnnotation... (may take a really long while)")
+                VariantTranscriptAnnotation.objects.filter(version=vav).update(impact=impact_case)
+
+    @staticmethod
+    def _get_case(old_new, field_name) -> Case:
+        return Case(*[When(**{field_name: k}, then=Value(v)) for k, v in old_new.items()])
