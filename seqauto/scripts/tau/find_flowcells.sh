ca3ba48c567ff1cbd9e8cd418c45f9846255dde0
Dave Lawrence <davmlaw@gmail.com>
2021-05-31 10:22:42 +0930

    Only need 5 not 6 (and they had second sequencing_stats dir)

--- seqauto/scripts/tau/find_flowcells.sh
+++ seqauto/scripts/tau/find_flowcells.sh
@@ -2,5 +2,5 @@
 
 # Only look in idt_* dirs - as that's where new stuff is put
 # RTAComplete.txt is in 4_QC/sequencing_stats so need to drop down 2 dirs (with dirname)
-find /tau/data/clinical_hg38 -maxdepth 6 -name "RTAComplete.txt" -exec dirname {} \; | xargs dirname | xargs dirname
+find /tau/data/clinical_hg38 -maxdepth 5 -name "RTAComplete.txt" -exec dirname {} \; | xargs dirname | xargs dirname
 
