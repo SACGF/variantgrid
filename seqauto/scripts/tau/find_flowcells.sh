e199bade1bfdfb3331bfb873d66913651bf40310
Dave Lawrence <davmlaw@gmail.com>
2021-05-31 10:13:00 +0930

    SA Path #155 - look for RTAComplete.txt

--- seqauto/scripts/tau/find_flowcells.sh
+++ seqauto/scripts/tau/find_flowcells.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 # Only look in idt_* dirs - as that's where new stuff is put
-find /tau/data/clinical_hg38 -maxdepth 3 -name "SampleSheet.csv" -exec dirname {} \;
+# RTAComplete.txt is in 4_QC/sequencing_stats so need to drop down 2 dirs (with dirname)
+find /tau/data/clinical_hg38 -maxdepth 6 -name "RTAComplete.txt" -exec dirname {} \; | xargs dirname | xargs dirname
 
--- seqauto/scripts/test/find_flowcells.sh
+++ seqauto/scripts/test/find_flowcells.sh
@@ -3,5 +3,6 @@
 TEST_DATA_DIR=$(dirname ${BASH_SOURCE[0]})/../../test_data
 TEST_DATA_DIR=$(cd "${TEST_DATA_DIR}"; pwd) # Absolute path
 
-find ${TEST_DATA_DIR}/clinical_hg38 -maxdepth 3 -name "SampleSheet.csv" -exec dirname {} \;
+# RTAComplete.txt is in 4_QC/sequencing_stats so need to drop down 2 dirs (with dirname)
+find ${TEST_DATA_DIR}/clinical_hg38 -maxdepth 6 -name "RTAComplete.txt" -exec dirname {} \; | xargs dirname | xargs dirname
 
