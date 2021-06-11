a56377355bfc5fe795d444872c2fb3d59be614e1
David Lawrence <davmlaw@gmail.com>
2021-06-11 10:57:38 +0930

    Whoops accidentally change for testing

--- scripts/dbscripts/pgsql_database_create.sql
+++ scripts/dbscripts/pgsql_database_create.sql
@@ -1,10 +1,10 @@
 -- Need Case Insensitive Text extension, install globally
 CREATE EXTENSION IF NOT EXISTS citext;
-CREATE DATABASE snpdb2;
+CREATE DATABASE snpdb;
 CREATE USER snpdb WITH PASSWORD 'snpdb';
-GRANT ALL PRIVILEGES ON DATABASE snpdb2 to snpdb;
+GRANT ALL PRIVILEGES ON DATABASE snpdb to snpdb;
 ALTER USER snpdb CREATEDB;
 
 -- Install extension install on snpdb database
-\connect snpdb2;
+\connect snpdb;
 CREATE EXTENSION IF NOT EXISTS citext;
