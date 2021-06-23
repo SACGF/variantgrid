8ccbb72cf497d2e2fb8fc632cbfb316cfd92ee0b
David Lawrence <davmlaw@gmail.com>
2021-06-23 11:21:49 +0930

    #373 - be able to delete in steps

--- snpdb/management/commands/delete_unused_variants.py
+++ snpdb/management/commands/delete_unused_variants.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 import logging
+import numpy as np
 
 from annotation.models import VariantAnnotationVersion, AnnotationRangeLock
 from snpdb.models import GenomeBuild, Variant
@@ -10,11 +11,18 @@ class Command(BaseCommand):
         Until 210622 - (PythonKnownVariantsImporter v.16) we used to insert a reference variant (alt='=') for each ALT
         We also didn't have a way to delete variants that are no longer referenced
     """
+    def add_arguments(self, parser):
+        # Usually an annotation range lock is 100k, so you'd expect 50k ref variants in there.
+        # So steps=20 will look in a 5k range and probably delete 2.5k Variant and 2.5k VariantZygosityCount
+        parser.add_argument('--steps', type=int, default=20, required=False,
+                            help="Number of steps to take in between AnnotationRangeLock regions (which are ~100k)")
+
     def handle(self, *args, **options):
         # We want to do this in small batches - so use the variant annotation range locks which are all approx the same
         # size (even if a big gap between IDs)
         # Variants from different builds are mixed up together so just do all builds
         # Any overlaps will be quick as not much to delete
+        steps = options["steps"]
 
         arl_qs = AnnotationRangeLock.objects.all()
         total = arl_qs.count()
@@ -24,20 +32,26 @@ class Command(BaseCommand):
             perc = 100 * i / total
             print(f"{perc:.2f}% done - doing step: {range_lock}")
 
-            # Skip the range lock min/max variant as that's protected so can't delete anyway
-            qs = Variant.objects.filter(pk__gt=range_lock.min_variant_id, pk__lt=range_lock.max_variant_id)
-            qs = qs.filter(classification__isnull=True,
-                           clinvar__isnull=True,
-                           varianttag__isnull=True,
-                           variantallele__isnull=True,
-                           cohortgenotype__isnull=True,
-                           # Avoid deleting those that are are within range in another build
-                           min_variant__isnull=True,
-                           max_variant__isnull=True)
-            _, details = qs.delete()
-            num_deleted = details.get('snpdb.Variant', 0)
-            print(details)
-            total_deleted += num_deleted
+            linspace = np.linspace(range_lock.min_variant_id, range_lock.max_variant_id, steps + 1).astype(int)
+            for s in range(steps):
+                start = linspace[s]
+                end = linspace[s+1]
+                print(f"{start} - {end} ({end-start})")
+                # Skip the range lock min/max variant as that's protected so can't delete anyway
+                qs = Variant.objects.filter(pk__gt=start, pk__lt=end)
+                qs = qs.filter(classification__isnull=True,
+                               clinvar__isnull=True,
+                               varianttag__isnull=True,
+                               variantallele__isnull=True,
+                               cohortgenotype__isnull=True,
+                               variantcollectionrecord__isnull=True,
+                               # Avoid deleting those that are are within range in another build
+                               min_variant__isnull=True,
+                               max_variant__isnull=True)
+                _, details = qs.delete()
+                num_deleted = details.get('snpdb.Variant', 0)
+                print(details)
+                total_deleted += num_deleted
 
         print(f"Total deleted: {total_deleted}")
         print("You should probably run:")
