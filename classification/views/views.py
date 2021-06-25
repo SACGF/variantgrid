11ad49d17bcca4cb21ba44157b98192c0a4421c6
TheMadBug <jimmy.andrews@gmail.com>
2021-06-25 10:21:03 +1000

    Remove date sort ordering on diff page

--- classification/views/views.py
+++ classification/views/views.py
@@ -321,20 +321,17 @@ def view_classification_diff(request):
         compare_all = ClassificationModification.latest_for_user(user=request.user, allele=ref.record.variant.allele, published=True)
         # filter out variant we're comparing with, make it last in calculation
         latest_others_for_variant = [vcm for vcm in compare_all if vcm.classification.id != ref.record.id]
-        latest_others_for_variant.sort(key=lambda cm:cm.curated_date_check, reverse=True)
         compare_all = [ref.modification] + latest_others_for_variant
         records = compare_all
 
     elif request.GET.get('variant'):
         variant_id = int(request.GET.get('variant'))
         compare_all = ClassificationModification.latest_for_user(user=request.user, allele=Variant.objects.get(pk=variant_id).allele, published=True)
-        compare_all.sort(key=lambda cm: cm.curated_date_check, reverse=True)
         records = compare_all
 
     elif request.GET.get('allele'):
         allele_id = int(request.GET.get('allele'))
         compare_all = ClassificationModification.latest_for_user(user=request.user, allele=Allele.objects.get(pk=allele_id), published=True)
-        compare_all.sort(key=lambda cm: cm.curated_date_check, reverse=True)
         records = compare_all
 
     elif cids := request.GET.get('cids'):
