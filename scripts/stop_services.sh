8d639b147662136f5061ce2e6c5cc556bfcc5fd4
David Lawrence <davmlaw@gmail.com>
2021-07-05 10:45:49 +0930

    VG Private #2760 - Celery beat service

--- scripts/stop_services.sh
+++ scripts/stop_services.sh
@@ -9,6 +9,6 @@ service celeryd_web_workers stop
 service celeryd_variant_id_single_worker stop
 service celeryd_scheduling_single_worker stop
 service celeryd_seqauto_single_worker stop
-#service celeryd_beat stop
+service celeryd_beat stop
 
 pkill gunicorn
