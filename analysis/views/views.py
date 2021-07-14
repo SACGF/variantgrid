877fb759aa642dc150cff103549a133d8c58f4c9
Dave Lawrence <davmlaw@gmail.com>
2021-07-14 13:35:57 +0930

    #435 - order analysis template versions

--- analysis/views/views.py
+++ analysis/views/views.py
@@ -238,7 +238,8 @@ def stand_alone_analysis_editor_and_grid(request, analysis_id):
 
 def analysis_templates_list(request, pk):
     analysis_template = AnalysisTemplate.get_for_user(request.user, pk)
-    context = {"analysis_template": analysis_template}
+    context = {"analysis_template": analysis_template,
+               "analysis_template_versions": analysis_template.analysistemplateversion_set.order_by("-pk")}
     return render(request, 'analysis/analysis_templates_list.html', context)
 
 
