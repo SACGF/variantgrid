f39a49b31ed882a66d7fb506e9dd4258464c2338
Dave Lawrence <davmlaw@gmail.com>
2021-07-22 02:07:30 +0000

    finish settings move

--- variantgrid/settings/env/vgaws.py
+++ variantgrid/settings/env/vgaws.py
@@ -55,11 +55,4 @@ URLS_NAME_REGISTER.update({"sequencing_data": False})
 SOMALIER["enabled"] = True
 SOMALIER["annotation_base_dir"] = os.path.join(ANNOTATION_REFERENCE_BASE_DIR, "somalier")
 
-_VG_COM_STATIC_FILES_DIR = os.path.join(VARIANTGRID_APP_DIR, "static_files", "variantgrid_com_static")
-STATICFILES_DIRS = (_VG_COM_STATIC_FILES_DIR,) + STATICFILES_DIRS
-
-_VG_COM_TEMPLATES_DIR = os.path.join(VARIANTGRID_APP_DIR, "templates/variantgrid_com_templates")
-if os.path.exists(_VG_COM_TEMPLATES_DIR):
-    TEMPLATES[0]["DIRS"].insert(0, _VG_COM_TEMPLATES_DIR)
-
 UPLOAD_ENABLED = True
