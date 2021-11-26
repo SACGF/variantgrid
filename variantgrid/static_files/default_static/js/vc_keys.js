cd025f9a61aae72d93d4904ed2470145ff44eaa9
TheMadBug <jimmy.andrews@gmail.com>
2021-11-26 16:15:01 +1100

    Also allow divs… this is actually the last whitelist change (promise)

--- variantgrid/static_files/default_static/js/vc_keys.js
+++ variantgrid/static_files/default_static/js/vc_keys.js
@@ -214,7 +214,7 @@ let EKey = (function() {
     
 })();
 
-EKey.HTML_WHITE_LIST = new Set(['BR','B','I','OL','UL','LI','U','SUP','SUB', 'SPAN']);
+EKey.HTML_WHITE_LIST = new Set(['BR','B','I','OL','UL','LI','U','SUP','SUB', 'SPAN', 'DIV']);
 
 // UNSPECIFIED STRENGTH HANDLING
 EKey.critValues = {
