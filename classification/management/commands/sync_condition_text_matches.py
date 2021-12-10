10184414c1a4b58d417ef7b2ec188b1d9e86c3c7
TheMadBug <jimmy.andrews@gmail.com>
2021-12-10 14:50:28 +1100

    Provide command line to remove orphon condition text matching.

--- classification/management/commands/sync_condition_text_matches.py
+++ classification/management/commands/sync_condition_text_matches.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+from typing import Dict
+
 from django.core.management.base import BaseCommand
 
 from classification.models import ConditionText, sync_all_condition_resolutions_to_classifications
@@ -13,12 +16,27 @@ class Command(BaseCommand):
         parser.add_argument('--reset', action='store_true', default=False)
         parser.add_argument('--clear', action='store_true', default=False)
         parser.add_argument('--classifications', action='store_true', default=False)
+        parser.add_argument('--orphans', actions='store_true', default=False)
 
     def handle(self, *args, **options):
         if options["classifications"]:
             print("Updating classifications")
             sync_all_condition_resolutions_to_classifications()
             print("Complete")
+            return
+        if options["orphans"]:
+            print("Removing orphans")
+            for ct in ConditionText.objects.all():
+                classifications_for_gene_symbol: Dict[str, int] = defaultdict(int)
+                for ctm in list(ct.conditiontextmatch_set.all()):
+                    if gene_symbol := ctm.gene_symbol:
+                        if ctm.classification_id:
+                            classifications_for_gene_symbol[gene_symbol] += 1
+                        else:
+                            classifications_for_gene_symbol[gene_symbol] += 0  # register that we have the gene symbol
+                for gene_symbol, count in classifications_for_gene_symbol.items():
+                    if count == 0:
+                        ct.conditiontextmatch_set.filter(gene_symbol=gene_symbol).delete()
             return
 
         if options["reset"]:
