2f0cf06b072da3b6bce81743852d04d86d3fdcfb
TheMadBug <jimmy.andrews@gmail.com>
2021-12-17 14:42:34 +1030

    Don’t notify about brand new Allele becoming concordant

--- classification/models/clinical_context_models.py
+++ classification/models/clinical_context_models.py
@@ -211,6 +211,7 @@ class ClinicalContext(FlagsMixin, TimeStampedModel):
         old_status = self.status
         new_status = self.calculate_status()
         is_significance_change = old_status != new_status
+        is_simple_new = old_status is None and new_status == ClinicalContextStatus.CONCORDANT
         allele_url = get_url_from_view_path(self.allele.get_absolute_url())
 
         ongoing_import = ClassificationImportRun.ongoing_imports()
@@ -256,7 +257,7 @@ class ClinicalContext(FlagsMixin, TimeStampedModel):
             # don't send out any signals if calculation is delayed, we don't want to trigger anything until we complete
             return
 
-        if settings.DISCORDANCE_ENABLED and is_significance_change:
+        if settings.DISCORDANCE_ENABLED and is_significance_change and not is_simple_new:
             nb = NotificationBuilder("ClinicalContext changed")
             nb.add_markdown(
                 f":fire_engine: ClinicalGrouping for allele <{allele_url}|{allele_url}> changed from {old_status} -> {new_status} "
