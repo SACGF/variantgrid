be575a833c9c8e667a7a042ca84119b1ff701e85
Dave Lawrence <davmlaw@gmail.com>
2022-01-07 07:24:50 +0000

    server changes

--- snpdb/management/commands/delete_unused_variants.py
+++ snpdb/management/commands/delete_unused_variants.py
@@ -1,3 +1,4 @@
+import random
 import numpy as np
 from django.core.management.base import BaseCommand
 
@@ -15,15 +16,20 @@ class Command(BaseCommand):
         # So steps=20 will look in a 5k range and probably delete 2.5k Variant and 2.5k VariantZygosityCount
         parser.add_argument('--steps', type=int, default=20, required=False,
                             help="Number of steps to take in between AnnotationRangeLock regions (which are ~100k)")
+        parser.add_argument('--min-variant', type=int, required=False)
 
     def handle(self, *args, **options):
         # We want to do this in small batches - so use the variant annotation range locks which are all approx the same
         # size (even if a big gap between IDs)
-        # Variants from different builds are mixed up together so just do all builds
+        # Variants from different builds are mixed up together so just do 37 (won't be much of 38 that is not overlapping)
         # Any overlaps will be quick as not much to delete
         steps = options["steps"]
+        min_variant = options["min_variant"]
 
-        arl_qs = AnnotationRangeLock.objects.all()
+        check_ref = True 
+        arl_qs = AnnotationRangeLock.objects.filter(version__genome_build__name='GRCh37')
+        if min_variant:
+            arl_qs = arl_qs.filter(min_variant__gt=min_variant)
         total = arl_qs.count()
         print(f"Deleting unused variants in {total} steps...")
         total_deleted = 0
@@ -47,18 +53,24 @@ class Command(BaseCommand):
                                                                  varianttag__isnull=True,
                                                                  variantallele__isnull=True,
                                                                  cohortgenotype__isnull=True,
+                                                                 createdmanualvariant__isnull=True,
                                                                  min_variant__isnull=True,
                                                                  max_variant__isnull=True)
+                unused_variant_ids = list(unused_variants_qs.values_list("pk", flat=True))
+                if unused_variant_ids and check_ref:
+                    print(f"{len(unused_variant_ids)=}")
+                    num_ref = unused_variants_qs.filter(alt__seq='=').count()
+                    print(f"check... {num_ref=}")
+                    check_ref = False
 
                 # If there is a VariantCollectionRecord, but no sample, the analysis won't work anyway
-                unused_variant_ids = list(unused_variants_qs.values_list("pk", flat=True))
                 for klass in [VariantCollectionRecord, VariantZygosityCount, VariantAnnotation, VariantTranscriptAnnotation]:
                     qs = klass.objects.filter(variant_id__gt=start, variant_id__lt=end, variant_id__in=unused_variant_ids)
                     num_deleted = qs._raw_delete(qs.db)
                     print(f"{klass}: deleted {num_deleted} records")
 
                 variants_deleted = unused_variants_qs._raw_delete(unused_variants_qs.db)
-                print(f"{variants_deleted=}")
                 total_deleted += variants_deleted
+                print(f"{variants_deleted=} ({total_deleted=})")
 
         print(f"Total deleted: {total_deleted}")
