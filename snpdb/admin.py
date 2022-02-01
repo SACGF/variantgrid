e2f9c9efda4d82e2a5b5ad3e1553814b226cbf8e
TheMadBug <jimmy.andrews@gmail.com>
2022-02-01 12:33:39 +1100

    Tidy up of admin (columns for XUserSettings)

--- snpdb/admin.py
+++ snpdb/admin.py
@@ -3,14 +3,15 @@ from typing import List
 from django.contrib import admin, messages
 from django.contrib.admin.widgets import AdminTextInputWidget
 from django.db.models import QuerySet
+from unidecode import unidecode
 
 from snpdb import models
 from snpdb.admin_utils import ModelAdminBasics, GuardedModelAdminBasics, admin_list_column, \
     admin_action
 from snpdb.liftover import liftover_alleles
-from snpdb.models import Allele, VariantAllele, ClinVarKey, ClinVarKeyExcludePattern
+from snpdb.models import Allele, VariantAllele, ClinVarKey, ClinVarKeyExcludePattern, UserSettingsOverride, \
+    LabUserSettingsOverride, OrganizationUserSettingsOverride, UserPageAck, Organization, Lab
 from snpdb.models.models_genome import GenomeBuild
-from snpdb.models_admin_forms import LabAdmin, OrganizationAdmin
 
 
 class AlleleClingenFilter(admin.SimpleListFilter):
@@ -30,6 +31,7 @@ class AlleleClingenFilter(admin.SimpleListFilter):
         return queryset
 
 
+@admin.register(Allele)
 class AlleleAdmin(ModelAdminBasics):
 
     list_display = ('pk', 'clingen_allele', 'variants')
@@ -81,6 +83,7 @@ class DefaultBuildFilter(admin.SimpleListFilter):
         return queryset
 
 
+@admin.register(UserSettingsOverride)
 class UserSettingsOverrideAdmin(ModelAdminBasics):
     list_display = ('user', 'default_lab', 'default_genome_build', 'email_weekly_updates', 'email_discordance_updates')
     list_filter = (DefaultBuildFilter,)
@@ -88,7 +91,20 @@ class UserSettingsOverrideAdmin(ModelAdminBasics):
     actions = ["export_as_csv"]
 
 
-class UserPageAck(ModelAdminBasics):
+@admin.register(LabUserSettingsOverride)
+class LabUserSettingsOverrideAdmin(ModelAdminBasics):
+    list_display = ('lab', 'default_genome_build', 'email_weekly_updates', 'email_discordance_updates')
+    actions = ["export_as_csv"]
+
+
+@admin.register(OrganizationUserSettingsOverride)
+class OrganizationUserSettingsOverrideAdmin(ModelAdminBasics):
+    list_display = ('organization', 'default_genome_build', 'email_weekly_updates', 'email_discordance_updates')
+    actions = ["export_as_csv"]
+
+
+@admin.register(UserPageAck)
+class UserPageAckAdmin(ModelAdminBasics):
     list_display = ('user', 'page_id')
 
 
@@ -100,6 +116,7 @@ class ClinVarKeyExcludePatternAdmin(admin.TabularInline):
     }
 
 
+@admin.register(ClinVarKey)
 class ClinVarKeyAdmin(ModelAdminBasics):
     list_display = ('id', 'created', 'modified')
     inlines = (ClinVarKeyExcludePatternAdmin,)
@@ -135,9 +152,109 @@ class ClinVarKeyAdmin(ModelAdminBasics):
         }, **kwargs)
 
 
-admin.site.register(models.Allele, AlleleAdmin)
+def make_code_friendly(text: str) -> str:
+    """
+    convert accented characters to non-accented counterparts
+    lower case, replace - and spaces with underscores
+    remove anything that's then not a-z or underscore
+    """
+    text = unidecode(text) \
+        .lower() \
+        .replace('-', '_').replace(' ', '_')
+    return re.sub(r'[^a-z0-9_]', '', text)
+
+
+@admin.register(Lab)
+class LabAdmin(ModelAdminBasics):
+    list_per_page = 200
+    list_display = ('name', 'group_name', 'organization', 'state', 'country',
+                    'external', 'clinvar_key', 'upload_location', 'upload_auto_pattern', 'classification_config')
+
+    fieldsets = (
+        ('Basic', {'fields': ('name', 'group_name', 'organization')}),
+        ('Position', {'fields': ('city', 'state', 'country', 'lat', 'long')}),
+        ('Style', {'fields': ('url', 'css_class')}),
+        ('Submissions', {'fields': ('classification_config', 'upload_location', 'upload_auto_pattern', 'external', 'clinvar_key')})
+    )
+
+    def is_readonly_field(self, f) -> bool:
+        if f.name in ('clinvar_key', 'organization', 'state', 'country'):
+            return False
+        return super().is_readonly_field(f)
+
+    def get_form(self, request, obj=None, **kwargs):
+
+        return super(LabAdmin, self).get_form(request, obj, widgets={
+            'name': admin.widgets.AdminTextInputWidget(),
+            'group_name': admin.widgets.AdminTextInputWidget(),
+            'city': admin.widgets.AdminTextInputWidget(),
+            'lat': admin.widgets.AdminTextInputWidget(),
+            'long': admin.widgets.AdminTextInputWidget(),
+            'url': admin.widgets.AdminURLFieldWidget(),
+            'css_class': admin.widgets.AdminTextInputWidget(),
+            'upload_location': admin.widgets.AdminTextInputWidget(),
+            'upload_auto_pattern': admin.widgets.AdminTextInputWidget()
+        }, **kwargs)
+
+    def fix_group_name(self, request, queryset):
+        safety_reg = re.compile(r'^[a-z0-9_]*$')
+        fixed = 0
+        already_good = 0
+        lab: Lab
+        for lab in queryset:
+            org_group_name = lab.organization.group_name
+            if not lab.group_name or not safety_reg.match(lab.group_name) or not lab.group_name.startswith(org_group_name):
+                lab.group_name = org_group_name + '/' + make_code_friendly(lab.name)
+                lab.save()
+                fixed = fixed + 1
+            else:
+                already_good = already_good + 1
+        self.message_user(request, f"{fixed} updated, {already_good} no change required")
+
+    fix_group_name.short_description = 'Fix group name'
+
+    actions = [fix_group_name]
+
+
+@admin.register(Organization)
+class OrganizationAdmin(ModelAdminBasics):
+
+    list_display = ('name', 'group_name', 'classification_config')
+
+    fieldsets = (
+        ('Basic', {'fields': ('name', 'short_name', 'group_name', 'active')}),
+        ('Submissions', {'fields': ('classification_config', )})
+    )
+
+    def fix_group_name(self, request, queryset):
+        org: Organization
+        safety_reg = re.compile(r'^[a-z0-9_]*$')
+        fixed = 0
+        already_good = 0
+        for org in queryset:
+            if not org.group_name or not safety_reg.match(org.group_name):
+                org.group_name = make_code_friendly(org.name)
+                org.save()
+                fixed = fixed + 1
+            else:
+                already_good = already_good + 1
+        self.message_user(request, f"{fixed} updated, {already_good} no change required")
+
+    fix_group_name.short_description = 'Fix group name'
+
+    actions = [fix_group_name]
+
+    def get_form(self, request, obj=None, **kwargs):
+        return super(OrganizationAdmin, self).get_form(request, obj, widgets={
+            'name': admin.widgets.AdminTextInputWidget(),
+            'short_name': admin.widgets.AdminTextInputWidget(),
+            'group_name': admin.widgets.AdminTextInputWidget()
+
+        }, **kwargs)
+
+
+
 admin.site.register(models.CachedGeneratedFile, ModelAdminBasics)
-admin.site.register(models.ClinVarKey, ClinVarKeyAdmin)
 admin.site.register(models.Cohort, ModelAdminBasics)
 admin.site.register(models.CohortGenotypeCollection, ModelAdminBasics)
 admin.site.register(models.CohortSample, ModelAdminBasics)
@@ -147,13 +264,9 @@ admin.site.register(models.CustomColumnsCollection, ModelAdminBasics)
 admin.site.register(models.GenomeBuild, ModelAdminBasics)
 admin.site.register(models.GenomicIntervalsCollection, ModelAdminBasics)
 admin.site.register(models.GlobalSettings, ModelAdminBasics)
-admin.site.register(models.Lab, LabAdmin)
 admin.site.register(models.LabHead, ModelAdminBasics)
 admin.site.register(models.LabProject)
-admin.site.register(models.LabUserSettingsOverride, ModelAdminBasics)
 admin.site.register(models.Manufacturer)
-admin.site.register(models.OrganizationUserSettingsOverride, ModelAdminBasics)
-admin.site.register(models.Organization, OrganizationAdmin)
 admin.site.register(models.Project, ModelAdminBasics)
 admin.site.register(models.Sample, ModelAdminBasics)
 admin.site.register(models.SampleLabProject)
@@ -163,9 +276,7 @@ admin.site.register(models.SiteMessage, ModelAdminBasics)
 admin.site.register(models.State)
 admin.site.register(models.Tag, ModelAdminBasics)
 admin.site.register(models.Trio, ModelAdminBasics)
-admin.site.register(models.UserSettingsOverride, UserSettingsOverrideAdmin)
 admin.site.register(models.VCF, GuardedModelAdminBasics)
 admin.site.register(models.VCFSourceSettings, ModelAdminBasics)
 admin.site.register(models.VCFTag, ModelAdminBasics)
 admin.site.register(models.VariantGridColumn, ModelAdminBasics)
-admin.site.register(models.UserPageAck, UserPageAck)
