a23eca64af1bdf9d8a71a485c077dbde46f96af1
David Lawrence <davmlaw@gmail.com>
2022-02-08 14:39:15 +1030

    Do in batch SQL calls

--- classification/migrations/0060_one_off_set_alleles.py
+++ classification/migrations/0060_one_off_set_alleles.py
@@ -8,15 +8,22 @@ def _set_alleles(apps, schema_editor):
     Classification = apps.get_model("classification", "Classification")
     VariantAllele = apps.get_model("snpdb", "VariantAllele")
 
-    print("About to assign alleles to all classifications with variants")
+    print("Loading variant alleles")
+    va_qs = VariantAllele.objects.filter(variant__varianttag__isnull=False)
+    allele_by_variant = dict(va_qs.values_list("variant_id", "allele_id"))
 
-    # don't want to trigger update date for changing this
+    print("About to assign alleles to all classifications with variants")
+    classifications = []
     for (count, c) in enumerate(Classification.objects.filter(allele__isnull=True, variant__isnull=False)):
-        if va := VariantAllele.objects.filter(variant=c.variant).first():
-            c.allele = va.allele
-            c.save(update_fields=['allele'])
+        if allele_id := allele_by_variant.get(c.variant_id):
+            c.allele_id = allele_id
+            classifications.append(c)
         if count % 1000 == 0:
             print(f"Processed {count} classifications")
+
+    if classifications:
+        print("Performing bulk update")
+        Classification.objects.bulk_update(classifications, fields=["allele_id"], batch_size=2000)
 
     print("Done")
 
