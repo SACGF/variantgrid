e1599628673028ac8573dc4e623e90a39b33c861
TheMadBug <jimmy.andrews@gmail.com>
2022-02-28 10:58:30 +1100

    Stop the ontology search from throwing exceptions for things that have a semicolon in them

--- ontology/models/ontology_search.py
+++ ontology/models/ontology_search.py
@@ -18,12 +18,15 @@ class SearchResponseOntology(SearchResponseRecordAbstract[OntologyTerm]):
 def search_ontology(sender: Any, search_input: SearchInput, **kwargs) -> SearchResponse:
     response: SearchResponse[OntologyTerm] = SearchResponse(SearchResponseOntology)
 
-    if search_input.matches_pattern(r"\w+:\s*.*"):
-        response.mark_valid_search()
-
-        term = OntologyTerm.get_or_stub(search_input.search_string)
-        if not term.is_stub:
-            response.add(term)
+    try:
+        if search_input.matches_pattern(r"\w+:\s*.*"):
+            term = OntologyTerm.get_or_stub(search_input.search_string)
+            response.mark_valid_search()
+            if not term.is_stub:
+                response.add(term)
+    except ValueError:
+        # might not be a valid ontology, there's a lot of text that passes the search_input
+        pass
 
     if search_input.matches_has_alpha() and not GeneSymbol.objects.filter(symbol=search_input.search_string).exists():
         qs = OntologyTerm.objects.exclude(ontology_service=OntologyService.HGNC). \
