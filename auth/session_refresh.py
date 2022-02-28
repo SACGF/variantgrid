5790cc4517d56bb40ff90db35c1400c62754720f
TheMadBug <jimmy.andrews@gmail.com>
2022-02-28 16:47:44 +1100

    Fix for ODIC with latest django

--- auth/session_refresh.py
+++ auth/session_refresh.py
@@ -3,7 +3,12 @@ from mozilla_django_oidc.middleware import SessionRefresh
 
 class VariantGridSessionRefresh(SessionRefresh):
 
+    def is_ajax(self, request):
+        # request.is_ajax has been deprecated, is somewhat jQuery dependant
+        # we need to start making sure all our ajax targets are in some kind of pattern
+        return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
+
     def is_refreshable_url(self, request):
-        if '/api/' in request.path or request.is_ajax():
+        if '/api/' in request.path or VariantGridSessionRefresh.is_ajax(request):
             return False
         return super().is_refreshable_url(request)
