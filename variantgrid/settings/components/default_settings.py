fbd8e4261a30a6c55cc2d9c31543e566aa83c01b
David Lawrence <davmlaw@gmail.com>
2022-05-02 15:18:42 +0930

    Don't remove whitespace from code

--- variantgrid/settings/components/default_settings.py
+++ variantgrid/settings/components/default_settings.py
@@ -578,7 +578,7 @@ HTML_MINIFY = True
 EXCLUDE_FROM_MINIFYING = (
     '^media/',  # Died in Django 4: FileResponse instance has no 'content' attribute. Use 'streaming_content' instead.
 )
-EXCLUDE_TAGS_FROM_MINIFYING = ("pre", "script", "textarea", "nomin")
+EXCLUDE_TAGS_FROM_MINIFYING = ("pre", "script", "textarea", "nomin", "code")
 
 ROOT_URLCONF = 'variantgrid.urls'
 
