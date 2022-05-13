0b1099f1856de3fb84278f44b9fa0e20c5ec7aae
Dave Lawrence <davmlaw@gmail.com>
2022-05-13 11:45:15 +0930

    #514 dbNSFP 4.3

--- annotation/annotation_data/generate_annotation/dbnsfp_grch37_strip.sh
+++ annotation/annotation_data/generate_annotation/dbnsfp_grch37_strip.sh
@@ -6,32 +6,29 @@ set -e
 
 # Get dbNSFP fields used by VariantGrid - run python3 manage.py shell
 # In [12]: ",".join(ColumnVEPField.get_source_fields(vep_plugin='d'))                                                                                                                                     
-# Out[12]: 'MutationAssessor_pred,Polyphen2_HVAR_pred,REVEL_score,MutationTaster_pred,GERP++_RS,FATHMM_pred,Interpro_domain,CADD_raw,CADD_phred'
 
 # Get column names from dbNSFP data file
-# df = pd.read_csv("./dbNSFP4.0a.grch37.gz", sep='\t', index_col=None, nrows=0)
-# vep_fields = 'MutationAssessor_pred,Polyphen2_HVAR_pred,REVEL_score,MutationTaster_pred,GERP++_RS,FATHMM_pred,Interpro_domain,CADD_raw,CADD_phred'
+# df = pd.read_csv("./dbNSFP4.3a_grch37.gz", sep='\t', index_col=None, nrows=0)
+# vep_fields = 'GERP++_RS,Interpro_domain,CADD_raw_rankscore,REVEL_rankscore,BayesDel_noAF_rankscore,ClinPred_rankscore,VEST4_rankscore,MetaLR_rankscore'
 # columns = ['ref', 'alt',  'hg19_chr', 'hg19_pos(1-based)', 'Ensembl_transcriptid'] + vep_fields.split(",")
 # cols = []
 # for i in columns:
 #    cols.append(list(df.columns).index(i) + 1)
 # ",".join([str(c) for c in sorted(cols)])
-# Out[7]: '3,4,8,9,15,48,55,60,63,79,102,104,137,373'
+# columns are: '3,4,8,9,15,69,74,84,104,107,119,156,640'
 
-#if [[ ! -e dbNSFP4.0a.zip ]]; then
-#  wget ftp://dbnsfp:dbnsfp@dbnsfp.softgenetics.com/dbNSFP4.0a.zip
-#  unzip dbNSFP4.0a.zip
-#fi
+
+# Download 4.3 from https://sites.google.com/site/jpopgen/dbNSFP
 
 # https://m.ensembl.org/info/docs/tools/vep/script/vep_plugins.html#dbnsfp
-# zcat dbNSFP4.1a_variant.chr1.gz | head -n1 > h
-# zgrep -h -v ^#chr dbNSFP4.1a_variant.chr* | awk '$8 != "." ' | sort -T /path/to/tmp_folder -k8,8 -k9,9n - | cat h - | bgzip -c > dbNSFP4.1a_grch37.gz
-# tabix -s 8 -b 9 -e 9 dbNSFP4.1a_grch37.gz
+# zcat dbNSFP4.3a_variant.chr1.gz | head -n1 > h
+# zgrep -h -v ^#chr dbNSFP4.3a_variant.chr* | awk '$8 != "." ' | sort -T /path/to/tmp_folder -k8,8 -k9,9n - | cat h - | bgzip -c > dbNSFP4.3a_grch37.gz
+# tabix -s 8 -b 9 -e 9 dbNSFP4.3a_grch37.gz
 
-IN_FILE=dbNSFP4.0a.grch37.gz
-OUT_FILE=dbNSFP4.0a.grch37.stripped.gz
+IN_FILE=dbNSFP4.3a_grch37.gz
+OUT_FILE=dbNSFP4.3a_grch37.stripped.gz
 
 # Header needs to start with #
-(echo -n "#" ; zcat ${IN_FILE} | cut -f 3,4,8,9,15,48,55,60,63,79,102,104,137,373 ) | bgzip > ${OUT_FILE}
+(echo -n "#" ; zcat ${IN_FILE} | cut -f 3,4,8,9,15,69,74,84,104,107,119,156,640 ) | bgzip > ${OUT_FILE}
 tabix -s 3 -b 4 -e 4 ${OUT_FILE} # cols are: 1=ref, 2=alt, 3=chr, 4=pos
 
--- annotation/annotation_data/generate_annotation/dbnsfp_grch38_strip.sh
+++ annotation/annotation_data/generate_annotation/dbnsfp_grch38_strip.sh
@@ -6,29 +6,30 @@ set -e
 
 # Get dbNSFP fields used by VariantGrid - run python3 manage.py shell
 # In [12]: ",".join(ColumnVEPField.get_source_fields(vep_plugin='d'))                                                                                                                                     
-# Out[12]: 'MutationAssessor_pred,Polyphen2_HVAR_pred,REVEL_score,MutationTaster_pred,GERP++_RS,FATHMM_pred,Interpro_domain,CADD_raw,CADD_phred'
 
 # Get column names from dbNSFP data file
-# df = pd.read_csv("./dbNSFP4.0a.grch38.gz", sep='\t', index_col=None, nrows=0)
-# vep_fields = 'MutationAssessor_pred,Polyphen2_HVAR_pred,REVEL_score,MutationTaster_pred,GERP++_RS,FATHMM_pred,Interpro_domain,CADD_raw,CADD_phred'
+# df = pd.read_csv("./dbNSFP4.3a_grch38.gz", sep='\t', index_col=None, nrows=0)
+# vep_fields = 'GERP++_RS,Interpro_domain,CADD_raw_rankscore,REVEL_rankscore,BayesDel_noAF_rankscore,ClinPred_rankscore,VEST4_rankscore,MetaLR_rankscore'
 # columns = ['#chr', 'pos(1-based)', 'ref', 'alt', 'Ensembl_transcriptid'] + vep_fields.split(",")
 # cols = []
 # for i in columns:
 #    cols.append(list(df.columns).index(i) + 1)
 # ",".join([str(c) for c in sorted(cols)])
-# Out[18]: '1,2,3,4,15,48,55,60,63,79,102,104,137,373'
+# columns are: '1,2,3,4,15,69,74,84,104,107,119,156,640'
+
+# Download 4.3 from https://sites.google.com/site/jpopgen/dbNSFP
 
 # https://m.ensembl.org/info/docs/tools/vep/script/vep_plugins.html#dbnsfp
 
-# zcat dbNSFP4.0a_variant.chr1.gz | head -n1 > h
-# zgrep -h -v ^#chr dbNSFP4.0a_variant.chr* | sort -T /path/to/tmp_folder -k1,1 -k2,2n - | cat h - | bgzip -c > dbNSFP4.0a.gz
-# tabix -s 1 -b 2 -e 2 dbNSFP4.0a.gz
+# zcat dbNSFP4.3a_variant.chr1.gz | head -n1 > h
+# zgrep -h -v ^#chr dbNSFP4.3a_variant.chr* | sort -T /path/to/tmp_folder -k1,1 -k2,2n - | cat h - | bgzip -c > dbNSFP4.3a_grch38.gz
+# tabix -s 1 -b 2 -e 2 dbNSFP4.3a_grch38.gz
 
 
-IN_FILE=dbNSFP4.0a.grch38.gz
-OUT_FILE=dbNSFP4.0a.grch38.stripped.gz
+IN_FILE=dbNSFP4.3a.grch38.gz
+OUT_FILE=dbNSFP4.03.grch38.stripped.gz
 
 # Header needs to start with #
-(echo -n "#" ; zcat ${IN_FILE} | cut -f 1,2,3,4,15,48,55,60,63,79,102,104,137,373 ) | bgzip > ${OUT_FILE}
+(echo -n "#" ; zcat ${IN_FILE} | cut -f 1,2,3,4,15,69,74,84,104,107,119,156,640 ) | bgzip > ${OUT_FILE}
 tabix -s 1 -b 2 -e 2 ${OUT_FILE} # cols are: 1=chr, 2=pos
 
