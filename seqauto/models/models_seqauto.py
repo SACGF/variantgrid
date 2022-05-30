0facf4a94bc880dc328b54019b57d3e070fa42bf
Dave Lawrence <davmlaw@gmail.com>
2022-05-30 11:11:39 +0930

    sa path server changes

--- seqauto/models/models_seqauto.py
+++ seqauto/models/models_seqauto.py
@@ -900,7 +900,7 @@ class SampleSheetCombinedVCFFile(SeqAutoRecord):
         pattern_list = None
         if enrichment_kit:
             kit_name = enrichment_kit.name
-            if "ffpe" not in kit_name.lower() and sample_sheet.sequencing_run.name.contains("_FFPE_"):
+            if "ffpe" not in kit_name.lower() and "_FFPE_" in sample_sheet.sequencing_run.name:
                 kit_name += "_ffpe"
             pattern_list = settings.SEQAUTO_COMBINED_VCF_PATTERNS_FOR_KIT.get(kit_name)
 
