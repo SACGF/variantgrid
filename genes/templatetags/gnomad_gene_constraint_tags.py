d12ad9a1b362138e88da54009502fd246d9c1103
David Lawrence <davmlaw@gmail.com>
2022-08-10 16:14:23 +0930

    #636 - new dbNSFP gene annotation

--- genes/templatetags/gnomad_gene_constraint_tags.py
+++
@@ -1,7 +0,0 @@
-from django.template import Library
-
-register = Library()
-
-@register.inclusion_tag("genes/tags/gnomad_gene_constraint.html")
-def gnomad_gene_constraint(gene_symbol):
-    return {"gene_symbol": gene_symbol}
