ad24265a4087fc9a24f8de807b3acf910958709d
TheMadBug <jimmy.andrews@gmail.com>
2022-08-19 10:13:27 +1000

    Fixes for flag health check

--- flags/models/flag_health_check.py
+++ flags/models/flag_health_check.py
@@ -4,7 +4,7 @@ from typing import Dict, List, Optional, Iterable
 
 from django.db.models import Q
 
-from flags.models import FlagType, FlagComment, Flag, FlagStatus
+from flags.models import FlagType, FlagComment, Flag, FlagStatus, FlagResolution
 
 
 class FlagDelta:
@@ -26,11 +26,11 @@ def flag_chanced_since(since: datetime, flag_types: Optional[Iterable[FlagType]]
 
     new_comments = FlagComment.objects.filter(time_range_q)
     # find all flags that have had comments added in the time range
-    modified_flags = Flag.objects.filter(pk__in=new_comments.values_list("flag_id", flat=True))
+    modified_flags = Flag.objects.filter(pk__in=new_comments.values_list("flag_id", flat=True), flag_type__in=flag_types)
     # now want to exclude flags that were created and then closed in that time
     modified_flags = modified_flags.exclude(
         Q(created__gte=since) & Q(resolution__status__in=[FlagStatus.CLOSED, FlagStatus.REJECTED])
-    ).filter(flag_type__in=flag_types).select_related("resolution", "flag_type")
+    ).select_related("resolution", "flag_type")
 
     # also need to filter out flags that were open before since and are still open now (likewise for closed)
     # to know that we need to find the most recent flag comment from before closing, and the last flag comment
@@ -48,21 +48,26 @@ def flag_chanced_since(since: datetime, flag_types: Optional[Iterable[FlagType]]
         else:
             raise ValueError("Diff must be 1 or -1")
 
-    def is_open_status(flag_status: FlagStatus):
+    def is_open_resolution(resolution: FlagResolution):
         # consider closed and rejected the same
-        return flag_status == FlagStatus.OPEN
+        # not just when the flag changed
+        return resolution.status == FlagStatus.OPEN
 
     for flag in modified_flags:
-        is_flag_open_now = is_open_status(flag.resolution.status)
+        is_flag_open_now = is_open_resolution(flag.resolution)
         # grab latest comment from before since
-        if reference_comment := FlagComment.objects.filter(flag=flag, created__lte=since).select_related('resolution').order_by('-created').first():
-            if not reference_comment.resolution or is_open_status(reference_comment.resolution.status) != is_flag_open_now:
+        if state_prior_to_since := FlagComment.objects.filter(flag=flag, created__lte=since, resolution__isnull=False).select_related('resolution').order_by('-created').first():
+            # TODO It would be better if FlagComment resolution was not nullable and always reflected the state of the flag at the time
+            # but instead we get the first comment with a resolution (FlagComments should always have a resolution when they open)
+            was_open_then = is_open_resolution(state_prior_to_since.resolution)
+
+            if was_open_then != is_flag_open_now:
                 # flag existed before since date, see if the status has changed after the since date
                 count_delta(flag.flag_type, 1 if is_flag_open_now else -1)
         else:
-            # no reference comment, so flag was created after the since date
-            # don't count it if we created and closed it within the window
+            # flag didn't exist before since date, so report it if it's open and ignore it if it closed itself
             if is_flag_open_now:
+                # flag is currently open and was created within the time window, report it
                 count_delta(flag.flag_type, 1)
 
     return sorted(flag_deltas.values())
