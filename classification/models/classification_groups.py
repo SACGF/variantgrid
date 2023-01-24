b494eb7a0af197b7dbc74e27eac25a04f33f0721
TheMadBug <jimmy.andrews@gmail.com>
2023-01-25 09:35:03 +1100

    remove dead code

--- classification/models/classification_groups.py
+++ classification/models/classification_groups.py
@@ -275,30 +275,12 @@ class ClassificationGroup:
     @cached_property
     def variant_sort(self) -> str:
         return self.c_hgvs.sort_str
-        # genome_build = GenomeBuildManager.get_current_genome_build()
-        # for cm in self.modifications:
-        #     if variant := cm.classification.get_variant_for_build(genome_build):
-        #         return variant.sort_string
-        # return ""
+
 
     @property
     def c_hgvs(self) -> CHGVS:
         return self.c_hgvses[0]
 
-    # @cached_property
-    # def flag_types(self) -> List[List[str]]:
-    #     FLAG_TYPES = [
-    #         classification_flag_types.matching_variant_flag,
-    #         classification_flag_types.matching_variant_warning_flag,
-    #         classification_flag_types.transcript_version_change_flag
-    #     ]
-    #     types: Set[str] = set()
-    #     flags_qs = self.most_recent.classification.flag_collection_safe.flags(only_open=True)
-    #     for type_n_res in flags_qs.filter(flag_type__in=FLAG_TYPES).values_list('flag_type', 'resolution'):
-    #         types.add(type_n_res[0] + '.' + type_n_res[1])
-    #     type_list = list(types)
-    #     type_list.sort()
-    #     return [part.split(".") for part in type_list]
 
     @property
     def has_matching_error(self) -> bool:
