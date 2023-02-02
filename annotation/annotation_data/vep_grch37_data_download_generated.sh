95913ebbe0087be5e51b3c3a04273985334b8924
Dave Lawrence <davmlaw@gmail.com>
2023-02-02 16:15:49 +1030

    #52 - add other files to download

--- annotation/annotation_data/vep_grch37_data_download_generated.sh
+++ annotation/annotation_data/vep_grch37_data_download_generated.sh
@@ -8,27 +8,32 @@ BASE_URL=https://variantgrid.com/download/annotation/VEP/annotation_data/GRCh37
 
 cd annotation_data/GRCh37
 
+wget ${BASE_URL}/CosmicCodingMuts_v95_20211101_grch37.normal.vcf.gz
+wget ${BASE_URL}/CosmicCodingMuts_v95_20211101_grch37.normal.vcf.gz.tbi
+wget ${BASE_URL}/TOPMED_GRCh37.vcf.gz
+wget ${BASE_URL}/TOPMED_GRCh37.vcf.gz.tbi
+wget ${BASE_URL}/UK10K_COHORT.20160215.sites.vcf.gz
+wget ${BASE_URL}/UK10K_COHORT.20160215.sites.vcf.gz.tbi
 wget ${BASE_URL}/dbNSFP4.3a.grch37.stripped.gz
 wget ${BASE_URL}/dbNSFP4.3a.grch37.stripped.gz.tbi
-
 wget ${BASE_URL}/dbscSNV1.1_GRCh37.txt.gz
 wget ${BASE_URL}/dbscSNV1.1_GRCh37.txt.gz.tbi
-
 wget ${BASE_URL}/gnomad2.1.1_GRCh37_combined_af.vcf.bgz
 wget ${BASE_URL}/gnomad2.1.1_GRCh37_combined_af.vcf.bgz.tbi
-
+wget ${BASE_URL}/gnomad_GRCh37_af_greater_than_5.contigs.vcf.bgz
+wget ${BASE_URL}/gnomad_GRCh37_af_greater_than_5.contigs.vcf.bgz.tbi
+wget ${BASE_URL}/hg19.100way.phastCons.bw
+wget ${BASE_URL}/hg19.100way.phyloP100way.bw
 wget ${BASE_URL}/hg19.phastCons46way.placental.bw
 wget ${BASE_URL}/hg19.phyloP46way.placental.bw
+wget ${BASE_URL}/mastermind_cited_variants_reference-2022.04.02-grch37.vcf.gz
+wget ${BASE_URL}/mastermind_cited_variants_reference-2022.04.02-grch37.vcf.gz.tbi
+wget ${BASE_URL}/repeatmasker_hg19.bed.gz
+wget ${BASE_URL}/repeatmasker_hg19.bed.gz.tbi
+wget ${BASE_URL}/spliceai_scores.raw.indel.hg19.vcf.gz
+wget ${BASE_URL}/spliceai_scores.raw.indel.hg19.vcf.gz.tbi
+wget ${BASE_URL}/spliceai_scores.raw.snv.hg19.vcf.gz
+wget ${BASE_URL}/spliceai_scores.raw.snv.hg19.vcf.gz.tbi
 
-# Preprocessor files
-# gnomad_GRCh37_af_greater_than_5.contigs.vcf.bgz
-
-
-# Behind registration wall
-# spliceai_scores.raw.indel.hg19.vcf.gz
-# spliceai_scores.raw.indel.hg19.vcf.gz.tbi
-
-# spliceai_scores.raw.snv.hg19.vcf.gz
-# spliceai_scores.raw.snv.hg19.vcf.gz.tbi
 
 cd ../..
--- annotation/annotation_data/vep_grch38_data_download_generated.sh
+++ annotation/annotation_data/vep_grch38_data_download_generated.sh
@@ -8,29 +8,33 @@ BASE_URL=https://variantgrid.com/download/annotation/VEP/annotation_data/GRCh38
 
 cd annotation_data/GRCh38
 
+wget ${BASE_URL}/CosmicCodingMuts_v95_20211101_grch38.normal.vcf.gz
+wget ${BASE_URL}/CosmicCodingMuts_v95_20211101_grch38.normal.vcf.gz.tbi
+wget ${BASE_URL}/TOPMED_GRCh38_20180418.vcf.gz
+wget ${BASE_URL}/TOPMED_GRCh38_20180418.vcf.gz.tbi
+wget ${BASE_URL}/UK10K_COHORT.20160215.sites.GRCh38.vcf.gz
+wget ${BASE_URL}/UK10K_COHORT.20160215.sites.GRCh38.vcf.gz.tbi
 wget ${BASE_URL}/dbNSFP4.3a.grch38.stripped.gz
 wget ${BASE_URL}/dbNSFP4.3a.grch38.stripped.gz.tbi
-
 wget ${BASE_URL}/dbscSNV1.1_GRCh38.txt.gz
 wget ${BASE_URL}/dbscSNV1.1_GRCh38.txt.gz.tbi
-
-
 wget ${BASE_URL}/gnomad2.1.1_GRCh38_combined_af.vcf.bgz
 wget ${BASE_URL}/gnomad2.1.1_GRCh38_combined_af.vcf.bgz.tbi
-
 wget ${BASE_URL}/gnomad3.1_GRCh38_merged.vcf.bgz
 wget ${BASE_URL}/gnomad3.1_GRCh38_merged.vcf.bgz.tbi
-
-# Preprocessor files
-# gnomad_GRCh37_af_greater_than_5.contigs.vcf.bgz
-
-# Behind registration wall
-
-# annotation_data/GRCh38/CosmicCodingMuts_v95_20211101_grch38.normal.vcf.gz",
-#             "mastermind": "annotation_data/GRCh38/mastermind_cited_variants_reference-2022.04.02-grch38.vcf.gz",
-#             "maxentscan": "annotation_data/all_builds/maxentscan",
-#            "repeatmasker": "annotation_data/GRCh38/repeatmasker_hg38.bed.gz",
-#            "spliceai_snv": "annotation_data/GRCh38/spliceai_scores.raw.snv.hg38.vcf.gz",
-#            "spliceai_indel": "annotation_data/GRCh38/spliceai_scores.raw.indel.hg38.vcf.gz",
+wget ${BASE_URL}/gnomad_GRCh38_af_greater_than_5.contigs.vcf.bgz
+wget ${BASE_URL}/gnomad_GRCh38_af_greater_than_5.contigs.vcf.bgz.tbi
+wget ${BASE_URL}/hg38.phastCons100way.bw
+wget ${BASE_URL}/hg38.phastCons30way.bw
+wget ${BASE_URL}/hg38.phyloP100way.bw
+wget ${BASE_URL}/hg38.phyloP30way.bw
+wget ${BASE_URL}/mastermind_cited_variants_reference-2022.04.02-grch38.vcf.gz
+wget ${BASE_URL}/mastermind_cited_variants_reference-2022.04.02-grch38.vcf.gz.tbi
+wget ${BASE_URL}/repeatmasker_hg38.bed.gz
+wget ${BASE_URL}/repeatmasker_hg38.bed.gz.tbi
+wget ${BASE_URL}/spliceai_scores.raw.indel.hg38.vcf.gz
+wget ${BASE_URL}/spliceai_scores.raw.indel.hg38.vcf.gz.tbi
+wget ${BASE_URL}/spliceai_scores.raw.snv.hg38.vcf.gz
+wget ${BASE_URL}/spliceai_scores.raw.snv.hg38.vcf.gz.tbi
 
 cd ../..
