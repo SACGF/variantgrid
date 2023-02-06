e56102e5b12bea61adae96c9180c2718a72ae688
Dave Lawrence <davmlaw@gmail.com>
2023-02-07 09:42:15 +1030

    Install scripts - Comment out files available on web

--- annotation/annotation_data/vep_grch38_data_download_generated.sh
+++ annotation/annotation_data/vep_grch38_data_download_generated.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# Behind registration wall
-# CosmicCodingMuts.normal.grch37.vcf.gz
-# CosmicCodingMuts.normal.grch37.vcf.gz.tbi
+# Files commented out are available on the web (vep_grch38_data_download.sh)
 
 BASE_URL=https://variantgrid.com/download/annotation/VEP/annotation_data/GRCh38
 
@@ -10,10 +8,10 @@ cd annotation_data/GRCh38
 
 wget ${BASE_URL}/CosmicCodingMuts_v95_20211101_grch38.normal.vcf.gz
 wget ${BASE_URL}/CosmicCodingMuts_v95_20211101_grch38.normal.vcf.gz.tbi
-wget ${BASE_URL}/TOPMED_GRCh38_20180418.vcf.gz
-wget ${BASE_URL}/TOPMED_GRCh38_20180418.vcf.gz.tbi
-wget ${BASE_URL}/UK10K_COHORT.20160215.sites.GRCh38.vcf.gz
-wget ${BASE_URL}/UK10K_COHORT.20160215.sites.GRCh38.vcf.gz.tbi
+#wget ${BASE_URL}/TOPMED_GRCh38_20180418.vcf.gz
+#wget ${BASE_URL}/TOPMED_GRCh38_20180418.vcf.gz.tbi
+#wget ${BASE_URL}/UK10K_COHORT.20160215.sites.GRCh38.vcf.gz
+#wget ${BASE_URL}/UK10K_COHORT.20160215.sites.GRCh38.vcf.gz.tbi
 wget ${BASE_URL}/dbNSFP4.3a.grch38.stripped.gz
 wget ${BASE_URL}/dbNSFP4.3a.grch38.stripped.gz.tbi
 wget ${BASE_URL}/dbscSNV1.1_GRCh38.txt.gz
@@ -24,10 +22,10 @@ wget ${BASE_URL}/gnomad3.1_GRCh38_merged.vcf.bgz
 wget ${BASE_URL}/gnomad3.1_GRCh38_merged.vcf.bgz.tbi
 wget ${BASE_URL}/gnomad_GRCh38_af_greater_than_5.contigs.vcf.bgz
 wget ${BASE_URL}/gnomad_GRCh38_af_greater_than_5.contigs.vcf.bgz.tbi
-wget ${BASE_URL}/hg38.phastCons100way.bw
-wget ${BASE_URL}/hg38.phastCons30way.bw
-wget ${BASE_URL}/hg38.phyloP100way.bw
-wget ${BASE_URL}/hg38.phyloP30way.bw
+#wget ${BASE_URL}/hg38.phastCons100way.bw
+#wget ${BASE_URL}/hg38.phastCons30way.bw
+#wget ${BASE_URL}/hg38.phyloP100way.bw
+#wget ${BASE_URL}/hg38.phyloP30way.bw
 wget ${BASE_URL}/mastermind_cited_variants_reference-2022.04.02-grch38.vcf.gz
 wget ${BASE_URL}/mastermind_cited_variants_reference-2022.04.02-grch38.vcf.gz.tbi
 wget ${BASE_URL}/repeatmasker_hg38.bed.gz
