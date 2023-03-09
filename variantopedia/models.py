73af4565a083d4e8aef74e096426a3f16c0bee97
Dave Lawrence <davmlaw@gmail.com>
2023-03-09 15:01:10 +1030

    sort lines

--- variantopedia/models.py
+++ variantopedia/models.py
@@ -1,25 +1,25 @@
 
 
 class SearchTypes:
+    CLASSIFICATION = "Classification"
+    CLINGEN_ALLELE_ID = "ClinGen Allele ID"
+    COHORT = "Cohort"
     COSMIC = "COSMIC"
     DBSNP = "dbSNP"
     EXPERIMENT = 'Experiment'
     EXTERNAL_PK = 'ExternalPK'
-    CLINGEN_ALLELE_ID = "ClinGen Allele ID"
-    COHORT = "Cohort"
     GENE = "Gene"
     GENE_SYMBOL = "Gene Symbol"
     HGVS = "HGVS"
     LAB = "Lab"
-    ORG = "Organisation"
     LOCUS = "Locus"
+    ONTOLOGY = "Ontology"
+    ORG = "Organisation"
     PATIENT = "Patient"
     PEDIGREE = "Pedigree"
     SAMPLE = "Sample"
-    VCF = "VCF"
     SEQUENCING_RUN = 'SequencingRun'
     TRANSCRIPT = "Transcript"
     UNKNOWN = "No known format"
     VARIANT = "Variant"
-    CLASSIFICATION = "Classification"
-    ONTOLOGY = "Ontology"
+    VCF = "VCF"
