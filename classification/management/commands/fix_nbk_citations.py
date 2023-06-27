fa4cefb46fcb4b4587854e1782409e5a7cc011f6
TheMadBug <jimmy.andrews@gmail.com>
2023-06-27 13:39:50 +1000

    Tidy up script for fixing NBK

--- classification/management/commands/fix_nbk_citations.py
+++ classification/management/commands/fix_nbk_citations.py
@@ -5,11 +5,8 @@ from library.guardian_utils import admin_bot
 
 class Command(BaseCommand):
 
-    def add_arguments(self, parser):
-        parser.add_argument('--delete', action='store_true', default=False)
-
     def handle(self, *args, **options):
-        for classification in Classification.objects.iterator():
+        for classification in Classification.objects.order_by('pk').iterator():
             has_nbk = False
             for ref in classification.db_refs:
                 if ref.get("db") == "HTTPS" and "www.ncbi.nlm.nih.gov/books" in ref.get("id"):
