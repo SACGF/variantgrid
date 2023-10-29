c3cac84f5371ee8a3fbc1f7b4c2aaee393a825cc
Bharath Reddy Kandula <65353049+Bharath-kandula@users.noreply.github.com>
2023-10-30 09:11:03 +1100

    #3530 added filters on ViewEvent Admin and modified View Metrics Page (#935)

--- eventlog/admin.py
+++ eventlog/admin.py
@@ -2,6 +2,54 @@ from django.contrib import admin
 
 from eventlog.models import ViewEvent
 from snpdb.admin_utils import ModelAdminBasics
+from django.contrib.auth.models import Group
+
+from snpdb.models import Lab, Organization
+
+
+class AdminUsers(admin.SimpleListFilter):
+    title = 'User Type'
+    parameter_name = 'user_type'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('exclude', 'Exclude Admin/Test Users'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'exclude':
+            groups_to_exclude = ['variantgrid/tester', 'variantgrid/bot']
+            groups = Group.objects.filter(name__in=groups_to_exclude)
+            return queryset.exclude(user__groups__in=groups).exclude(user__is_superuser=True)
+        return queryset
+
+
+class LabFilter(admin.SimpleListFilter):
+    title = 'Lab'
+    parameter_name = 'lab'
+
+    def lookups(self, request, model_admin):
+        labs = Lab.objects.all().values_list('group_name', 'name')
+        return [(group_name, name) for group_name, name in labs]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(user__groups__name=self.value())
+        return queryset
+
+
+class OrganizationFilter(admin.SimpleListFilter):
+    title = 'Organization'
+    parameter_name = 'organization'
+
+    def lookups(self, request, model_admin):
+        organizations = Organization.objects.all().values_list('group_name', 'name')
+        return [(group_name, name) for group_name, name in organizations]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(user__groups__name=self.value())
+        return queryset
 
 
 @admin.register(ViewEvent)
@@ -18,3 +66,8 @@ class ViewEventAdmin(ModelAdminBasics):
 
     list_display = ('pk', 'view_name', 'user', 'args', 'path', 'created')
     search_fields = ('view_name', )
+    list_filter = (
+        OrganizationFilter,
+        LabFilter,
+        AdminUsers
+    )
