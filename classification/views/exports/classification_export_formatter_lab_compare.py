d8d2e664383b76ee6ad09e26cbfb4eb7f8055077
Bharath Reddy Kandula <65353049+Bharath-kandula@users.noreply.github.com>
2023-12-12 14:33:20 +1100

    #3539 added extra columns to lap compare export (#955)
    * #3539 added extra columns to lap compare export
    
    * #3539 updated lab compare export labels
    
    * #3539 updated lab compare export labels

--- classification/views/exports/classification_export_formatter_lab_compare.py
+++ classification/views/exports/classification_export_formatter_lab_compare.py
@@ -1,4 +1,6 @@
-from typing import List
+import re
+from collections import defaultdict
+from typing import List, Optional
 
 from django.http import HttpRequest
 from django.urls.base import reverse
@@ -13,10 +15,16 @@ from library.utils import ExportRow, export_column, delimited_row
 
 class ClassificationlabCompareRow(ExportRow):
 
-    def __init__(self,  AlleleData, lab1, lab2):
-        self.AlleleData = AlleleData
-        self.lab1 = lab1
-        self.lab2 = lab2
+    def __init__(self,  allele_data: AlleleData, lab_1: Optional[str] = None, lab_2: Optional[str] = None,
+                 comment: Optional[str] = None, authorised_date: Optional[str] = None,
+                 created_date: Optional[str] = None, patient_id: Optional[str] = None):
+        self.AlleleData = allele_data.allele_id
+        self.lab1 = lab_1
+        self.lab2 = lab_2
+        self.comment = comment
+        self.authorised_date = authorised_date
+        self.created_date = created_date
+        self.Patient_id = patient_id
 
     @export_column("Allele URL")
     def allele_url(self):
@@ -31,7 +39,7 @@ class ClassificationlabCompareRow(ExportRow):
     def lab2_clinical_significance(self):
         return self.lab2
 
-    @export_column("Difference")
+    @export_column("Classification")
     def difference(self, lab_name1, lab_name2):
         if (self.lab1 != '' and self.lab2 != '') and self.lab1 == self.lab2:
             return 'Same'
@@ -43,6 +51,22 @@ class ClassificationlabCompareRow(ExportRow):
             return 'Different'
         elif self.lab1 == '' and self.lab2 == '':
             return 'No data'
+
+    @export_column("Authorised Date")
+    def authorised_date(self):
+        return self.authorised_date
+
+    @export_column("Created Date")
+    def created_date(self):
+        return self.created_date
+
+    @export_column("Other Variables")
+    def comment(self):
+        return ''
+
+    @export_column("Patient_Id")
+    def patient_id(self):
+        return self.Patient_id
 
 
 @register_classification_exporter("lab_compare")
@@ -63,7 +87,8 @@ class ClassificationExportInternalCompare(ClassificationExportFormatter):
     def header(self) -> List[str]:
         if self.classification_filter.include_sources and len(self.classification_filter.include_sources) == 2:
             lab_names = sorted([str(lab) for lab in self.classification_filter.include_sources])
-            return [delimited_row(['Allele URL', lab_names[0], lab_names[1], 'Difference'], ',')]
+            return [delimited_row(['Allele URL', 'Patient_Id', lab_names[0], lab_names[1], 'Classification',
+                                   'Authorised Date', 'Created Date', 'Other Variables'], ',')]
         else:
             raise ValueError("Must specify 2 labs to compare")
 
@@ -71,25 +96,100 @@ class ClassificationExportInternalCompare(ClassificationExportFormatter):
         return []
 
     def row(self, allele_data: AlleleData) -> List[str]:
+
+        lab1_comment_data = defaultdict(list)
+        lab2_comment_data = defaultdict(list)
+
         rows: List[str] = []
         lab1 = set()
         lab2 = set()
+        message = ''
+        patient_ids = set()
+        authorised_date_lab1 = set()
+        authorised_date_lab2 = set()
+        created_date_lab1 = set()
+        created_date_lab2 = set()
 
         if not allele_data.allele_id:
             return []
         if self.classification_filter.include_sources:
             lab_name1, lab_name2 = sorted(self.classification_filter.include_sources)
             for cm in allele_data.cms_regardless_of_issues:
-                if cm.lab == lab_name1 and cm.clinical_significance is not None:
-                    lab1.add(cm.get(SpecialEKeys.CLINICAL_SIGNIFICANCE))
-                if cm.lab == lab_name2 and cm.clinical_significance is not None:
-                    lab2.add(cm.get(SpecialEKeys.CLINICAL_SIGNIFICANCE))
+                authorised_by = cm.get('curation_verified_by', '')
+                authorised_date = cm.get(SpecialEKeys.CURATION_VERIFIED_DATE, '')
+                created_by = cm.get('curated_by', '')
+                created_date = cm.get(SpecialEKeys.CURATION_DATE, '')
+                patient_id = cm.get(SpecialEKeys.PATIENT_ID, '')
+                cs = cm.get(SpecialEKeys.CLINICAL_SIGNIFICANCE)
+                if patient_id != '':
+                    patient_ids.add(patient_id)
+                if cm.lab == lab_name1:
+                    if cs and cs != 'None':
+                        lab1.add(cs)
+                    for key, value in cm.evidence.items():
+                        lab1_comment_data[key].append(value)
+                    authorised_date_lab1.add(authorised_by + ' ' + authorised_date)
+                    created_date_lab1.add(created_by + ' ' + created_date)
+                if cm.lab == lab_name2:
+                    if cs and cs != 'None':
+                        lab2.add(cs)
+                    for key, value in cm.evidence.items():
+                        lab2_comment_data[key].append(value)
+                    authorised_date_lab2.add(authorised_by + ' ' + authorised_date)
+                    created_date_lab2.add(created_by + ' ' + created_date)
 
-            lab1_set = ','.join(lab1)
-            lab2_set = ','.join(lab2)
-            row = ClassificationlabCompareRow(AlleleData=allele_data.allele_id, lab1=lab1_set, lab2=lab2_set)
-            rows.append(delimited_row([row.allele_url(), row.lab_clinical_significance(),
-                                       row.lab2_clinical_significance(), row.difference(lab_name1, lab_name2)], ','))
+            lab1_set = ','.join(sorted(lab1))
+            lab2_set = ','.join(sorted(lab2))
+            patient_id_set = ','.join(sorted(patient_ids))
+            authorised_date_lab1_set = {str(x) for x in authorised_date_lab1}
+            authorised_date_lab2_set = {str(x) for x in authorised_date_lab2}
+            created_date_lab1_set = {str(x) for x in created_date_lab1}
+            created_date_lab2_set = {str(x) for x in created_date_lab2}
+
+            if authorised_date_lab1_set != authorised_date_lab2_set:
+                difference_authorised_by = 'Different'
+            else:
+                difference_authorised_by = 'Same'
+            if created_date_lab1_set != created_date_lab2_set:
+                difference_created_by = 'Different'
+            else:
+                difference_created_by = 'Same'
+
+            for key in lab1_comment_data.keys():
+                lab1_values = lab1_comment_data[key]
+                lab2_values = lab2_comment_data[key]
+
+                comment_data_same = self.compare_comment_data(lab1_values, lab2_values)
+
+                if not comment_data_same:
+                    message += f'{key}, '
+
+            row = ClassificationlabCompareRow(allele_data=allele_data, patient_id=patient_id_set, lab_1=lab1_set,
+                                              lab_2=lab2_set,
+                                              authorised_date=difference_authorised_by,
+                                              created_date=difference_created_by, comment=message
+                                              )
+            rows.append(delimited_row([row.allele_url(), row.patient_id(), row.lab_clinical_significance(),
+                                       row.lab2_clinical_significance(), row.difference(lab_name1, lab_name2),
+                                       row.authorised_date, row.created_date,
+                                       row.comment], ','))
         else:
             raise ValueError("Error comparing labs")
         return rows
+
+    def tokenize_text(self, text):
+        tokens = re.findall(r'\b\w+\b', text.lower())
+        return set(tokens)
+
+    def compare_comment_data(self, lab1_values, lab2_values):
+        lab1_tokens = set()
+        lab2_tokens = set()
+
+        for value in lab1_values:
+            lab1_tokens.update(self.tokenize_text(str(value).replace('\\n', ' ')))
+
+        for value in lab2_values:
+            lab2_tokens.update(self.tokenize_text(str(value).replace('\\n', ' ')))
+
+        return lab1_tokens == lab2_tokens
+
