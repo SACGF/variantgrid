99ba2d95c66aa592930e5b2649aa630a952fb0d3
bharath <brkandula96@gmail.com>
2023-12-22 09:45:46 +1100

    #3556 modified scv search notes

--- snpdb/signals/scv_search.py
+++ snpdb/signals/scv_search.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.core.exceptions import PermissionDenied
 
 from classification.models import ClinVarExport
 from library.enums.log_level import LogLevel
@@ -9,8 +10,8 @@ from snpdb.search import search_receiver, SearchInputInstance, SearchExample, HA
     search_type=ClinVarExport,
     pattern=HAS_SCV,
     example=SearchExample(
-        note="SCVnumber",
-        examples=["SCV123456789"]
+        note="SCV (Submitted ClinVar Record)",
+        examples=["SCV000056789"]
     )
 )
 def clinvar_export_search(search_input: SearchInputInstance):
@@ -23,7 +24,7 @@ def clinvar_export_search(search_input: SearchInputInstance):
         try:
             clinvar_export.clinvar_allele.clinvar_key.check_user_can_access(search_input.user)
             yield clinvar_export
-        except Exception:
+        except PermissionDenied:
             message = f"Only SCVs directly managed by your lab in {settings.SITE_NAME} are returned by search. Please check ClinVar directly for this SCV."
             yield SearchMessageOverall(message, LogLevel.WARNING)
     else:
