05f1ba128f5601a24ad4fa78ee6d04d939e19a4f
TheMadBug <jimmy.andrews@gmail.com>
2024-02-05 13:57:57 +1100

    Update ClinVar XML schema when parsing looking for Expert Panels

--- annotation/tests/test_clinvar_xml_parser.py
+++ annotation/tests/test_clinvar_xml_parser.py
@@ -3,7 +3,7 @@ import os
 from django.conf import settings
 from django.test import TestCase
 
-from annotation.clinvar_xml_parser_via_rcvs import ClinVarXmlParserViaRCVs
+from annotation.clinvar_xml_parser_via_vcv import ClinVarXmlParserViaVCV
 from annotation.models import ClinVarRecord, ClinVarRecordCollection
 
 TEST_DATA_DIR = os.path.join(settings.BASE_DIR, "annotation/tests/test_data")
@@ -11,23 +11,21 @@ TEST_DATA_DIR = os.path.join(settings.BASE_DIR, "annotation/tests/test_data")
 
 class TestClinVarXmlParser(TestCase):
 
-    def _check_valid(self, filename: str):
+    def _check_valid(self, clinvar_variation_id:int):
+        filename = f"clinvar_variant_{clinvar_variation_id}.xml"
         xml_file = os.path.join(TEST_DATA_DIR, filename)
         records: list[ClinVarRecord]
         with open(xml_file, "rb") as xml_input:
-            records = ClinVarXmlParserViaRCVs.load_from_input(xml_input)
+            records = ClinVarXmlParserViaVCV.load_from_input(xml_input)
 
-        clinvar_record_collection = ClinVarRecordCollection.objects.create(clinvar_variation_id=432)
+        clinvar_record_collection = ClinVarRecordCollection.objects.create(clinvar_variation_id=clinvar_variation_id)
         for record in records:
             print(record)
             record.clinvar_record_collection = clinvar_record_collection
             record.save()
 
-    def test_parsing_432(self):
-        self._check_valid("clinvar_variant_432.xml")
-
-    def test_parsing_41650(self):
-        self._check_valid("clinvar_variant_41650.xml")
+    def test_parsing_17662(self):
+        self._check_valid(clinvar_variation_id=17662)
 
     # def test_doing_it_live_41650(self):
     #     records = ClinVarXmlParser.load_from_clinvar_id(14650).all_records
