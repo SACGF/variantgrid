3efff4dea3ba0e527f04ec28b0ffa6d47a58cf7d
TheMadBug <jimmy.andrews@gmail.com>
2024-02-08 11:13:56 +1100

    Fix condition text matching (pointing to api-dev for now)
    # Conflicts:
    #	classification/models/condition_text_matching.py
    #	ontology/ontology_matching.py

---
+++ classification/models/condition_text_search.py
@@ -0,0 +1,16 @@
+import requests
+from library.constants import MINUTE_SECS
+from ontology.models import OntologyTerm
+
+
+def condition_text_search(search_text: str, row_limit: int = 10) -> list[OntologyTerm]:
+    response = requests.get(
+        f'https://api-dev.monarchinitiative.org/v3/api/search', {
+            "q": search_text,
+            "category": "biolink:Disease",
+            "limit": 10
+        }, timeout=MINUTE_SECS).json()
+
+    results = response.get("items")
+
+    return [OntologyTerm.get_or_stub(result.get("id")) for result in results]
