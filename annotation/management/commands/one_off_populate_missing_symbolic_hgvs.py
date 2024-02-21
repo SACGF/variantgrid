af1c59f8a4808c2021fd937ce725abf43192e8e2
Dave Lawrence <davmlaw@gmail.com>
2024-02-21 16:00:47 +1030

    #823 - rewrite to go through variant to reduce work done

--- annotation/management/commands/one_off_populate_missing_symbolic_hgvs.py
+++ annotation/management/commands/one_off_populate_missing_symbolic_hgvs.py
@@ -1,43 +1,64 @@
+import time
+
 from django.core.management import BaseCommand
 
-from annotation.models import VariantAnnotation, VariantTranscriptAnnotation, VariantAnnotationPipelineType, \
-    AbstractVariantAnnotation
+from annotation.models import VariantAnnotation, VariantTranscriptAnnotation
 from genes.hgvs import HGVSMatcher, HGVSException
-from snpdb.models import GenomeBuild
+from snpdb.models import GenomeBuild, Variant
 
 
 class Command(BaseCommand):
     """
         Ensembl VEP doesn't populate HGVS for SVs
         @see https://github.com/Ensembl/ensembl-vep/issues/1222#issuecomment-1188788614
+
+        The thing that made this slow was retrieving the ref/alt from genome sequences, so change to do it
+        By variant so we only need to do that once
+
     """
-    @staticmethod
-    def _calculate_hgvs(matcher, qs):
-        """ This can often be slow, eg 1/second """
-        va: AbstractVariantAnnotation
-        for va in qs:
+    def _update_annotation(self, v: Variant, variant_coordinate, matcher, records, klass):
+        va_list = []
+        for va in klass.objects.filter(variant=v):
             try:
-                variant_coordinate = va.variant.coordinate
                 if va.transcript_version:
                     transcript_accession = va.transcript_version.accession
                     va.hgvs_c = matcher.variant_coordinate_to_hgvs_variant(variant_coordinate, transcript_accession)
-                    yield va
+                    va_list.append(va)
             except (ValueError, HGVSException):
                 pass
 
-    def _update_annotation(self, genome_build, matcher, klass):
-        print(f"Setting hgvs_c on {genome_build.name} / {klass}")
+        records.extend(va_list)
+        self._bulk_update(klass, records)
 
-        va_qs = klass.objects.filter(version__genome_build=genome_build,
-                                     annotation_run__pipeline_type=VariantAnnotationPipelineType.CNV,
-                                     variant__alt__seq__startswith='<')
-        va_qs.select_related("variant", "variant__locus", "transcript_version")
-        klass.objects.bulk_update(self._calculate_hgvs(matcher, va_qs),
-                                  fields=["hgvs_c"], batch_size=50)
+    def _bulk_update(self, klass, records, min_batch_size=1000):
+        if len(records) >= min_batch_size:
+            klass.objects.bulk_update(records, fields=["hgvs_c"])
+            records.clear()
 
     def handle(self, *args, **options):
 
         for genome_build in GenomeBuild.builds_with_annotation():
             matcher = HGVSMatcher(genome_build)
-            self._update_annotation(genome_build, matcher, VariantAnnotation)
-            self._update_annotation(genome_build, matcher, VariantTranscriptAnnotation)
+            qs = Variant.objects.filter(Variant.get_contigs_q(genome_build))
+            symbolic_qs = qs.filter(alt__seq__startswith='<')
+            total = symbolic_qs.count()
+            print(f"{genome_build.name=} has {total} variants to update annotation")
+
+            va_list = []
+            vta_list = []
+
+            last_update = time.time()
+            for i, v in enumerate(symbolic_qs):
+                # Do this lookup of ref/alt once as it's expensive..
+                variant_coordinate = v.coordinate.as_external_explicit(genome_build)
+
+                self._update_annotation(v, variant_coordinate, matcher, va_list, VariantAnnotation)
+                self._update_annotation(v, variant_coordinate, matcher, vta_list, VariantTranscriptAnnotation)
+                now = time.time()
+                if now - last_update > 5:
+                    last_update = now
+                    print(f"{i} of {total} ({100 * i/total:.2f}%)")
+
+            # Insert any remaining
+            self._bulk_update(VariantAnnotation, va_list,1)
+            self._bulk_update(VariantTranscriptAnnotation, vta_list,1)
