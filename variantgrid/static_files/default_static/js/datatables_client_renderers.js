9eb953ce3721f6a500081bf74f56a33ede2b7217
Dave Lawrence <davmlaw@gmail.com>
2024-05-16 16:10:43 +0930

    issue #1193 - analysis audit log

--- variantgrid/static_files/default_static/js/datatables_client_renderers.js
+++ variantgrid/static_files/default_static/js/datatables_client_renderers.js
@@ -8,3 +8,24 @@ function renderGeneSymbol(geneSymbol, type, row) {
     });
     return link.prop('outerHTML');
 }
+
+function renderAnalysisAuditLogSummary(summary, type, row) {
+    let summaryText = summary['summary_text'];
+    if (summaryText) {
+        return "<span>" + summaryText + "</span>";
+    }
+    let changes = summary['changes'];
+    if (changes) {
+        const hideValues = new Set(['valid', 'status', 'version', 'shadow_color', 'appearance_version']);
+        let changesSummary = "<table class='table'>"
+        changesSummary += "<tr><th>field</th><th>old</th><th>new</th></tr>"
+        for (const [key, value] of Object.entries(changes)) {
+            if (!hideValues.has(key)) {
+                changesSummary += `<tr><td>${key}</td><td>${value[0]}</td><td>${value[1]}</td>`;
+            }
+        }
+        changesSummary += "</table>"
+        return changesSummary
+    }
+
+}
