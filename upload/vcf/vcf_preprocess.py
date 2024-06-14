c54d88d3548b1adacf423363f7fc2481c541b4f8
Dave Lawrence <davmlaw@gmail.com>
2024-06-14 14:18:42 +0930

    more debug info

--- upload/vcf/vcf_preprocess.py
+++ upload/vcf/vcf_preprocess.py
@@ -101,7 +101,10 @@ def preprocess_vcf(upload_step, remove_info=False, annotate_gnomad_af=False):
     logging.info("VCF PREPROCESS env:")
     logging.info("Manage command: %s", manage_command)
     print(f"Parent Python executable: {sys.executable}")
-    subprocess.run('python -c "import sys; print(f\'Subprocess Python executable: {sys.executable}\')"', shell=True)
+    p = subprocess.Popen('python -c "import sys; print(f\'Subprocess Python executable: {sys.executable}\')"',
+                         shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = p.communicate()
+    print(f"subprocess: {stdout=}, {stderr=}")
 
     read_variants_cmd = manage_command + ["vcf_clean_and_filter",
                                           "--genome-build",
