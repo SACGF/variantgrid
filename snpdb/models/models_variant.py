8df0ddff490ce8a08d53ad4bc464f8f6ae12fdee
TheMadBug <jimmy.andrews@gmail.com>
2024-06-19 10:23:29 +1000

    Big fix for allele error rendering

--- snpdb/models/models_variant.py
+++ snpdb/models/models_variant.py
@@ -1009,7 +1009,7 @@ class AlleleLiftover(models.Model):
     def error_tidy(self) -> str | dict:
         # If the JSON is just message=, grab the message
         if error_json := self.error:
-            if message := error_json.get("message") and len(error_json.keys()) == 1:
+            if (message := error_json.get("message")) and len(error_json.keys()) == 1:
                 return message
             return error_json
 
