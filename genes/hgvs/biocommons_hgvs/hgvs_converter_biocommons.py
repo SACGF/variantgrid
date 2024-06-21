1bf8755e7353c667ce31c0d3cab2a1d763689163
Dave Lawrence <davmlaw@gmail.com>
2024-06-21 16:59:30 +0930

    issue #1095 - be able to convert VCF to hgvs if you start with a contig name

--- genes/hgvs/biocommons_hgvs/hgvs_converter_biocommons.py
+++ genes/hgvs/biocommons_hgvs/hgvs_converter_biocommons.py
@@ -104,6 +104,12 @@ class BioCommonsHGVSConverter(HGVSConverter):
         else:
             assembly_name = genome_build.name
         self.babelfish = Babelfish(self.hdp, assembly_name)
+        # HACK waiting for babelfish to be patched - see https://github.com/SACGF/variantgrid/issues/1095
+        # TODO: Remove this once biocommons HGVS is patched
+        accessions = list(self.babelfish.name_to_ac_map.values())
+        self.babelfish.name_to_ac_map.update({ac: ac for ac in accessions})
+        # /End hack
+
         self.am = AssemblyMapper(self.hdp,
                                  assembly_name=genome_build.name,
                                  alt_aln_method='splign',
