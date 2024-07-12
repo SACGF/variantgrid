7ae5327eb5dbc1b385a9aa045d2f345f6fc59b48
Dave Lawrence <davmlaw@gmail.com>
2024-07-12 13:56:57 +0930

    issue #1093 - postgres15+ requires explicit permission for public schema - this needs to be on the database not global

--- scripts/dbscripts/pgsql_database_create.sql
+++ scripts/dbscripts/pgsql_database_create.sql
@@ -2,10 +2,10 @@
 CREATE EXTENSION IF NOT EXISTS citext;
 CREATE DATABASE snpdb;
 CREATE USER snpdb WITH PASSWORD 'snpdb';
-GRANT ALL ON SCHEMA public TO snpdb;  # Needed for Postgres 15+
 GRANT ALL PRIVILEGES ON DATABASE snpdb to snpdb;
 ALTER USER snpdb CREATEDB;
 
 -- Install extension install on snpdb database
 \connect snpdb;
 CREATE EXTENSION IF NOT EXISTS citext;
+GRANT ALL ON SCHEMA public TO snpdb;  # Needed for Postgres 15+
