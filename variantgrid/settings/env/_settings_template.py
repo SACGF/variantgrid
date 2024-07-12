ed386d7a4c7d668db2e14e83be57035b722cc446
Dave Lawrence <davmlaw@gmail.com>
2024-07-12 15:32:53 +0930

    issue #1093 - Add examples of how to customise VEP in settings template

--- variantgrid/settings/env/_settings_template.py
+++ variantgrid/settings/env/_settings_template.py
@@ -12,3 +12,28 @@ ALLOWED_HOSTS = ["localhost", WEB_HOSTNAME, WEB_IP]
 # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
 
 # PEDIGREE_MADELINE2_COMMAND = "madeline2"
+
+
+# Here is how you'd customise your VEP version and whether you want RefSeq etc:
+
+_different_vep_version = False
+if _different_vep_version:
+    ANNOTATION_VEP_VERSION = "112"
+    ANNOTATION_VEP_BASE_DIR = os.path.join(ANNOTATION_BASE_DIR, "VEP")
+    ANNOTATION_VEP_VERSION_DIR = os.path.join(ANNOTATION_VEP_BASE_DIR, "vep_code", ANNOTATION_VEP_VERSION)
+    ANNOTATION_VEP_CODE_DIR = os.path.join(ANNOTATION_VEP_VERSION_DIR, "ensembl-vep")
+    ANNOTATION_VEP_PLUGINS_DIR = os.path.join(ANNOTATION_VEP_VERSION_DIR, "plugins")
+
+_use_grch38 = False
+if _use_grch38:
+    ANNOTATION[BUILD_GRCH38]["enabled"] = True
+
+_use_refseq = True
+if _use_refseq:
+    ANNOTATION[BUILD_GRCH37].update({
+        "annotation_consortium": "RefSeq",
+    })
+    ANNOTATION[BUILD_GRCH38].update({
+        "annotation_consortium": "RefSeq",
+    })
+
