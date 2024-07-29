893cd265a5d9ce01f57d80d516c389820d11b551
Dave Lawrence <davmlaw@gmail.com>
2024-07-29 13:08:34 +0930

    Update script for current old versions

--- annotation/annotation_data/generate_annotation/generate_gnomad_upload_preprocess.sh
+++ annotation/annotation_data/generate_annotation/generate_gnomad_upload_preprocess.sh
@@ -10,15 +10,45 @@ if [[ -z ${VEP_ANNOTATION_DIR} ]]; then
   exit 1
 fi
 
+VARIANTGRID_DIR=$(dirname $(dirname $(dirname $(dirname ${BASH_SOURCE[0]}))))
+CHROM_MAPPING_DIR=${VARIANTGRID_DIR}/snpdb/genome
+
+GNOMAD_VERSION_GRCH37=2.1.1
+GNOMAD_VERSION_GRCH38=4.0
+
+
+
 GRCH37_DIR=${VEP_ANNOTATION_DIR}/annotation_data/GRCh37
+CHROM_MAPPING_37=${CHROM_MAPPING_DIR}/chrom_mapping_GRCh37.map
+OUTPUT_NAME_GRCH37=gnomad${GNOMAD_VERSION_GRCH37}_GRCh37_af_greater_than_5
+OUTPUT_AF_GRCH37=${GRCH37_DIR}/${OUTPUT_NAME_GRCH37}.vcf.gz
+OUTPUT_STRIPPED_GRCH37=${GRCH37_DIR}/${OUTPUT_NAME_GRCH37}.stripped.vcf.gz
+if [[ ! -e ${OUTPUT_STRIPPED_GRCH37} ]]; then
+  echo "Generating ${OUTPUT_STRIPPED_GRCH37}..."
+  if [[ ! -e ${OUTPUT_AF_GRCH37} ]]; then
+    echo "Extracting AF>0.05..."
+    bcftools view -i "AF>0.05" ${GRCH37_DIR}/gnomad${GNOMAD_VERSION_GRCH37}_GRCh37_combined_af.vcf.bgz --output-type z --output-file ${OUTPUT_AF_GRCH37}
+    tabix ${OUTPUT_AF_GRCH37}
+  fi
+  echo "Stripping INFO"
+  bcftools annotate --rename-chrs ${CHROM_MAPPING_37} -x ^INFO/AF --output-type z --output ${OUTPUT_STRIPPED_GRCH37} ${OUTPUT_AF_GRCH37}
+  tabix ${OUTPUT_STRIPPED_GRCH37}
+fi
+
+
 GRCH38_DIR=${VEP_ANNOTATION_DIR}/annotation_data/GRCh38
-
-bcftools view -i "AF>0.05" ${GRCH37_DIR}/gnomad2.1.1_GRCh37_combined_af.vcf.bgz --output-type z --output-file gnomad_GRCh37_af_greater_than_5.vcf.bgz
-tabix gnomad_GRCh37_af_greater_than_5.vcf.bgz
-bcftools annotate -x ^INFO/AF --rename-chrs chr_contig_GRCh37.map --output-type z --output gnomad_GRCh37_af_greater_than_5.contigs.vcf.bgz gnomad_GRCh37_af_greater_than_5.vcf.bgz
-tabix gnomad_GRCh37_af_greater_than_5.contigs.vcf.bgz
-
-bcftools view -i "AF>0.05" ${GRCH38_DIR}/gnomad3.1_GRCh38_merged.vcf.bgz --output-type z --output-file gnomad_GRCh38_af_greater_than_5.vcf.bgz
-tabix gnomad_GRCh38_af_greater_than_5.vcf.bgz
-bcftools annotate -x ^INFO/AF --rename-chrs chr_contig_GRCh38.map --output-type z --output gnomad_GRCh38_af_greater_than_5.contigs.vcf.bgz gnomad_GRCh38_af_greater_than_5.vcf.bgz
-tabix gnomad_GRCh38_af_greater_than_5.contigs.vcf.bgz
+CHROM_MAPPING_38=${CHROM_MAPPING_DIR}/chrom_mapping_GRCh38.map
+OUTPUT_NAME_GRCH38=gnomad${GNOMAD_VERSION_GRCH38}_GRCh38_af_greater_than_5
+OUTPUT_AF_GRCH38=${GRCH38_DIR}/${OUTPUT_NAME_GRCH38}.vcf.gz
+OUTPUT_STRIPPED_GRCH38=${GRCH38_DIR}/${OUTPUT_NAME_GRCH38}.stripped.vcf.gz
+if [[ ! -e ${OUTPUT_STRIPPED_GRCH38} ]]; then
+  echo "Generating ${OUTPUT_STRIPPED_GRCH38}..."
+  if [[ ! -e ${OUTPUT_AF_GRCH38} ]]; then
+    echo "Extracting AF>0.05..."
+    bcftools view -i "AF>0.05" ${GRCH38_DIR}/gnomad${GNOMAD_VERSION_GRCH38}_GRCh38_combined_af.vcf.bgz --output-type z --output-file ${OUTPUT_AF_GRCH38}
+    tabix ${OUTPUT_AF_GRCH38}
+  fi
+  echo "Stripping info"
+  bcftools annotate --rename-chrs ${CHROM_MAPPING_38} -x ^INFO/AF --output-type z --output ${OUTPUT_STRIPPED_GRCH38} ${OUTPUT_AF_GRCH38}
+  tabix ${OUTPUT_STRIPPED_GRCH38}
+fi
