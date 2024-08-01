8dfc8a119c614242082a095455e87039bc61e89d
TheMadBug <jimmy.andrews@gmail.com>
2024-08-01 15:51:29 +1000

    Testing having Missing 38 as a warning instead of an error

--- classification/models/classification_variant_info_models.py
+++ classification/models/classification_variant_info_models.py
@@ -228,7 +228,7 @@ _VALIDATION_TO_SEVERITY: [str, ALLELE_INFO_VALIDATION_SEVERITY] = {
     'transcript_version_change': "W",
     'gene_symbol_change': "W",
     'c_nomen_change': "E",
-    'missing_37': "E",
+    'missing_37': "W",
     'missing_38': "E",
     'cant_resolve_to_variant_coordinate': "E",
     'hgvs_issue': "E"  # deprecated as too generic
