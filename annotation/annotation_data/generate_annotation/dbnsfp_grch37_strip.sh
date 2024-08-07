5dbf042fedd7277567a400135e51984a6b028061
Dave Lawrence <davmlaw@gmail.com>
2024-08-07 15:51:10 +0930

    issue #1131 - update for dbNSFP4.8

--- annotation/annotation_data/generate_annotation/dbnsfp_grch37_strip.sh
+++ annotation/annotation_data/generate_annotation/dbnsfp_grch37_strip.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-# Download 4.5 from https://sites.google.com/site/jpopgen/dbNSFP
+# Download 4.8 from https://sites.google.com/site/jpopgen/dbNSFP
 
 # https://m.ensembl.org/info/docs/tools/vep/script/vep_plugins.html#dbnsfp
-# zcat dbNSFP4.5a_variant.chr1.gz | head -n1 > header.txt
-# mkdir /tmp/dbsnp37
-# zgrep -h -v ^#chr dbNSFP4.5a_variant.chr* | awk '$8 != "." ' | sort -T /tmp/dbsnp37 -k8,8 -k9,9n - | cat header.txt - | bgzip -c > dbNSFP4.5a_grch37.gz
-# tabix -s 8 -b 9 -e 9 dbNSFP4.5a.grch37.gz
+# zcat dbNSFP4.8a_variant.chr1.gz | head -n1 > header.txt
+# mkdir -p /tmp/dbnsfp37
+# zgrep -h -v ^#chr dbNSFP4.8a_variant.chr* | awk '$8 != "." ' | sort -T /tmp/dbnsfp37 -k8,8 -k9,9n - | cat header.txt - | bgzip -c > dbNSFP4.8a_grch37.gz
+# tabix -s 8 -b 9 -e 9 dbNSFP4.8a.grch37.gz
 
 
 # All of this python is just to get the columns used in cut and tabix args at bottom of this file
@@ -20,23 +20,26 @@ set -e
 # import pandas as pd
 # df = pd.read_csv("header.txt", sep='\t', index_col=None, nrows=0)
 # vep_fields = 'GERP++_RS,Interpro_domain,CADD_raw_rankscore,REVEL_rankscore,BayesDel_noAF_rankscore,ClinPred_rankscore,VEST4_rankscore,MetaLR_rankscore,Aloft_prob_Tolerant,Aloft_prob_Recessive,Aloft_prob_Dominant,Aloft_pred,Aloft_Confidence,AlphaMissense_rankscore,AlphaMissense_pred'
-# columns = ['ref', 'alt', 'aaref', 'aaalt', 'hg19_chr', 'hg19_pos(1-based)', 'Ensembl_transcriptid'] + vep_fields.split(",")
+# new_vep_fields = "CADD_raw,REVEL_score,BayesDel_noAF_score,ClinPred_score,ClinPred_pred,VEST4_score,MetaLR_score,MetaLR_pred,AlphaMissense_score"
+# columns = ['ref', 'alt', 'aaref', 'aaalt', 'hg19_chr', 'hg19_pos(1-based)', 'Ensembl_transcriptid'] + vep_fields.split(",") + new_vep_fields.split(",")
 # cols = []
 # for i in columns:
 #    cols.append(list(df.columns).index(i) + 1)
 # print(",".join([str(c) for c in sorted(cols)]))
-# columns are: '3,4,5,6,8,9,15,69,74,84,106,109,139,140,142,143,144,145,146,148,185,705'
+# columns are: '3,4,5,6,8,9,15,68,69,73,74,75,83,84,105,106,108,109,110,138,139,140,146,147,148,149,150,151,152,189,447'
 
 # Note: We can't do this per-contig then join them, as some variants switch contigs between builds
-CUT_COLUMNS="3,4,5,6,8,9,15,69,74,84,106,109,139,140,142,143,144,145,146,148,185,705"
+TMP_DIR=/tmp/dbnsfp37
+CUT_COLUMNS="3,4,5,6,8,9,15,68,69,73,74,75,83,84,105,106,108,109,110,138,139,140,146,147,148,149,150,151,152,189,447"
 SEQ_COL=5  # hg19_chr (after cut)
 POS_COL=6  # hg19_pos(1-based) (after cut)
 
-version=4.5a
+version=4.8a
 out_vcf=dbNSFP${version}_grch37.gz
 
 # cd /hpcfs/groups/phoenix-hpc-sacgf/reference/annotation/dbnsfp/dbnsfp4.5
 
+mkdir -p ${TMP_DIR}
 zcat dbNSFP${version}_variant.chr1.gz | head -n1 > h
 zgrep -h -v ^#chr dbNSFP${version}_variant.chr* | awk '$8 != "." ' | sort -T ${TMP_DIR} -k8,8 -k9,9n - | cat h - | bgzip -c > ${out_vcf}
 # Needs a '#' header
--- annotation/annotation_data/generate_annotation/dbnsfp_grch38_strip.sh
+++ annotation/annotation_data/generate_annotation/dbnsfp_grch38_strip.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-# Download 4.5 from https://sites.google.com/site/jpopgen/dbNSFP
+# Download 4.8 from https://sites.google.com/site/jpopgen/dbNSFP
 
 # https://m.ensembl.org/info/docs/tools/vep/script/vep_plugins.html#dbnsfp
 
-# zcat dbNSFP4.5a_variant.chr1.gz | head -n1 > header.txt
-# mkdir /tmp/dbsnp38
-# zgrep -h -v ^#chr dbNSFP4.5a_variant.chr* | sort -T /tmp/dbsnp38 -k1,1 -k2,2n - | cat header.txt - | bgzip -c > dbNSFP4.5a_grch38.gz
-# tabix -s 1 -b 2 -e 2 dbNSFP4.5a_grch38.gz
+# zcat dbNSFP4.8a_variant.chr1.gz | head -n1 > header.txt
+# mkdir -p /tmp/dbnsfp38
+# zgrep -h -v ^#chr dbNSFP4.8a_variant.chr* | sort -T /tmp/dbnsfp38 -k1,1 -k2,2n - | cat header.txt - | bgzip -c > dbNSFP4.8a_grch38.gz
+# tabix -s 1 -b 2 -e 2 dbNSFP4.8a_grch38.gz
 
 
 # All of this python is just to get the columns used in cut and tabix args
@@ -20,23 +20,29 @@ set -e
 # Get column names from dbNSFP data file
 # df = pd.read_csv("header.txt", sep='\t', index_col=None, nrows=0)
 # vep_fields = 'GERP++_RS,Interpro_domain,CADD_raw_rankscore,REVEL_rankscore,BayesDel_noAF_rankscore,ClinPred_rankscore,VEST4_rankscore,MetaLR_rankscore,Aloft_prob_Tolerant,Aloft_prob_Recessive,Aloft_prob_Dominant,Aloft_pred,Aloft_Confidence,AlphaMissense_rankscore,AlphaMissense_pred'
-# columns = ['#chr', 'pos(1-based)', 'ref', 'alt', 'aaref', 'aaalt', 'Ensembl_transcriptid'] + vep_fields.split(",")
+# new_vep_fields = "CADD_raw,REVEL_score,BayesDel_noAF_score,ClinPred_score,ClinPred_pred,VEST4_score,MetaLR_score,MetaLR_pred,AlphaMissense_score"
+# columns = ['#chr', 'pos(1-based)', 'ref', 'alt', 'aaref', 'aaalt', 'Ensembl_transcriptid'] + vep_fields.split(",") + new_vep_fields.split(",")
 # cols = []
 # for i in columns:
 #    cols.append(list(df.columns).index(i) + 1)
 # print(",".join([str(c) for c in sorted(cols)]))
-# columns are: '1,2,3,4,5,6,15,69,74,84,106,109,139,140,142,143,144,145,146,148,185,705'
+# columns are: '1,2,3,4,5,6,15,68,69,73,74,75,83,84,105,106,108,109,110,138,139,140,146,147,148,149,150,151,152,189,447'
 
-CUT_COLUMNS="1,2,3,4,5,6,15,69,74,84,106,109,139,140,142,143,144,145,146,148,185,705"
-OUT_FILE=dbNSFP4.5a.grch38.stripped
-TMP_DIR=/tmp # /hpcfs/groups/phoenix-hpc-sacgf/scratch/dbnsfp4.5_GRCh38
+TMP_DIR=/tmp/dbnsfp38
+CUT_COLUMNS="1,2,3,4,5,6,15,68,69,73,74,75,83,84,105,106,108,109,110,138,139,140,146,147,148,149,150,151,152,189,447"
+SEQ_COL=1  # chr
+POS_COL=2  # pos(1-based)
+
+version=4.8a
+out_file=dbNSFP${version}_grch38.stripped
+
 mkdir -p ${TMP_DIR}
 
 # Sort chromosomes individually as that's much more efficient
 cat header.txt | cut -f ${CUT_COLUMNS} > ${OUT_FILE}
 for chrom in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X Y; do
-    zgrep -h -v ^#chr dbNSFP4.5a_variant.chr${chrom}.gz | cut -f ${CUT_COLUMNS} | sort -T ${TMP_DIR} -k1,1 -k2,2n - >> ${OUT_FILE}
+    zgrep -h -v ^#chr dbNSFP${version}_variant.chr${chrom}.gz | cut -f ${CUT_COLUMNS} | sort -T ${TMP_DIR} -k${SEQ_COL},${SEQ_COL} -k${POS_COL},${POS_COL}n - >> ${out_file}
 done
 
-bgzip ${OUT_FILE}
-tabix -s 1 -b 2 -e 2 ${OUT_FILE}.gz # cols are: 1=chr, 2=pos
+bgzip ${out_file}
+tabix -s ${SEQ_COL} -b ${POS_COL} -e 2 ${out_file}.gz # cols are: 1=chr, 2=pos
