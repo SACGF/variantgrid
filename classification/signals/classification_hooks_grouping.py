43e2c802fefe40bd8ded3283412fe7453c11bceb
TheMadBug <jimmy.andrews@gmail.com>
2024-10-23 14:46:37 +1100

    Start doing ClassificationGrouping tidying up on the main thread (TODO improve with celery)

--- classification/signals/classification_hooks_grouping.py
+++ classification/signals/classification_hooks_grouping.py
@@ -2,10 +2,11 @@ from django.contrib.auth.models import User
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
 
-from classification.enums import ShareLevel
+from classification.enums import ShareLevel, SubmissionSource
 from classification.models import classification_post_publish_signal, Classification, ClassificationModification, \
-    classification_withdraw_signal, allele_info_changed_signal, ImportedAlleleInfo
+    classification_withdraw_signal, allele_info_changed_signal, ImportedAlleleInfo, ClassificationImportRun
 from classification.models.classification_grouping import ClassificationGrouping, ClassificationGroupingEntry
+from classification.models.classification_import_run import classification_imports_complete_signal
 from library.utils import DebugTimer
 
 ###
@@ -14,6 +15,17 @@ from library.utils import DebugTimer
 # call assign_grouping_for_classification, if it already exists and we just need to update it
 # call dirty_up
 ##
+
+def _instant_undirty_check():
+    if ClassificationImportRun.ongoing_imports():
+        pass
+    else:
+        ClassificationGrouping.update_all_dirty()
+
+
+@receiver(classification_imports_complete_signal, sender=ClassificationImportRun)
+def classification_imports_complete(sender, **kwargs):
+    ClassificationGrouping.update_all_dirty()
 
 
 @receiver(classification_post_publish_signal, sender=Classification)
@@ -24,15 +36,17 @@ def published(sender,
               previous_share_level: ShareLevel,
               user: User,
               debug_timer: DebugTimer,
+              source: SubmissionSource,
               **kwargs):  # pylint: disable=unused-argument
     # CLASSIFICATION PUBLISHED
     # we call assign_grouping_for_classification as we might have a new allele origin,
     # this might be the first time we're publishing... so dirty_up
     ClassificationGrouping.assign_grouping_for_classification(classification)
+    _instant_undirty_check()
 
 
 @receiver(pre_delete, sender=Classification)
-def deleted_variant(sender, instance: Classification, **kwargs):  # pylint: disable=unused-argument
+def deleting_classification(sender, instance: Classification, **kwargs):  # pylint: disable=unused-argument
     # CLASSIFICATION DELETED
     # when a classification is deleted, it will delete the corresponding ClassificationGroupingEntry
     # so we need to mark the Allele Origin grouping (which dirty up does)
@@ -45,6 +59,7 @@ def withdraw_changed(sender, classification: Classification, **kwargs):  # pylin
     # WITHDRAW
     if entry := ClassificationGroupingEntry.objects.filter(classification=classification).first():
         entry.dirty_up()
+        _instant_undirty_check()
 
 
 @receiver(allele_info_changed_signal, sender=ImportedAlleleInfo)
@@ -52,3 +67,4 @@ def allele_info_changed_update_classifications(sender, allele_info: ImportedAlle
     # CLASSIFICATION MATCHED TO AN ALLELE
     for classification in allele_info.classification_set.all():
         ClassificationGrouping.assign_grouping_for_classification(classification)
+    _instant_undirty_check()
