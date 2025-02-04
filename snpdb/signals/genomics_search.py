609fc8d2798f536dbeec694608f0099c5f274d7e
Dave Lawrence <davmlaw@gmail.com>
2025-02-04 16:12:28 +1030

    SACGF/variantgrid#1102 - don't show warning if result came in

--- snpdb/signals/genomics_search.py
+++ snpdb/signals/genomics_search.py
@@ -35,13 +35,15 @@ def contig_search(search_input: SearchInputInstance):
     q = Contig.get_q(search_input.search_string, case_sensitive=False)
     all_contigs = Contig.objects.filter(q)
     q_search_build = Q(genomebuildcontig__genome_build__in=search_input.genome_builds)
-    yield all_contigs.filter(q_search_build)
-    if other_contig := all_contigs.exclude(q_search_build).first():
-        contig_builds = other_contig.genomebuildcontig_set.all()
-        build_names = ", ".join(contig_builds.order_by("genome_build__name").values_list("genome_build__name", flat=True))
-        msg = f"contig={other_contig.refseq_accession} is from builds {build_names} which are not enabled/annotated."
-        yield SearchMessageOverall(msg, severity=LogLevel.WARNING)
-    elif re.match(r"^NC_\d+.\d$", search_input.search_string):
-        known_builds = GenomeBuild.get_known_builds_comma_separated_string()
-        msg = f"contig={search_input.search_string} not in known_builds: {known_builds}"
-        yield SearchMessageOverall(msg, severity=LogLevel.WARNING)
+    if contigs := list(all_contigs.filter(q_search_build)):
+        yield contigs
+    else:
+        if other_contig := all_contigs.exclude(q_search_build).first(): #
+            contig_builds = other_contig.genomebuildcontig_set.all()
+            build_names = ", ".join(contig_builds.order_by("genome_build__name").values_list("genome_build__name", flat=True))
+            msg = f"contig={other_contig.refseq_accession} is from builds {build_names} which are not enabled/annotated."
+            yield SearchMessageOverall(msg, severity=LogLevel.WARNING)
+        elif re.match(r"^NC_\d+.\d$", search_input.search_string):
+            known_builds = GenomeBuild.get_known_builds_comma_separated_string()
+            msg = f"contig={search_input.search_string} not in known_builds: {known_builds}"
+            yield SearchMessageOverall(msg, severity=LogLevel.WARNING)
