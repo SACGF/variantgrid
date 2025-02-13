e7bf359efc7775372aa42aff9ea701a2ffb6fd3e
TheMadBug <jimmy.andrews@gmail.com>
2025-02-14 10:10:02 +1100

    Fix a bug of updating a classification summary when pending changes flag closed

--- classification/signals/classification_hooks_pending_flags.py
+++ classification/signals/classification_hooks_pending_flags.py
@@ -9,6 +9,6 @@ from flags.models import flag_comment_action, Flag, FlagComment, FlagResolution
 def check_for_pending(sender, flag_comment: FlagComment, old_resolution: FlagResolution, **kwargs):
     if flag_comment.flag.flag_type == classification_flag_types.classification_pending_changes:
         if c := Classification.objects.filter(flag_collection__id=flag_comment.flag.collection_id).first():
-            print("UPDATING SUMMARY")
-            c.summary = ClassificationSummaryCalculator(ClassificationModification.objects.filter(classification=c, is_lab_published=True)).cache_dict()
+            # print("UPDATING SUMMARY")
+            c.summary = ClassificationSummaryCalculator(c.last_published_version).cache_dict()
             c.save(update_fields=["summary"])
