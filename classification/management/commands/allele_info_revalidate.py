0cd38dc618b89c21f3ea8a93afbb628a04b240aa
TheMadBug <jimmy.andrews@gmail.com>
2025-02-18 15:53:06 +1100

    Allow imported allele info to just re-run on records already marked as dirty

--- classification/management/commands/allele_info_revalidate.py
+++ classification/management/commands/allele_info_revalidate.py
@@ -5,10 +5,14 @@ from classification.models import ImportedAlleleInfo
 
 
 class Command(BaseCommand):
+
     def add_arguments(self, parser):
-        pass
+        parser.add_argument("dirty", action="store_true", help="Only validate Allele Infos already marked as dirty")
 
     def handle(self, *args, **options):
+        qs = ImportedAlleleInfo.objects.all()
+        if options["dirty"]:
+            qs = qs.filter(dirty_message__isnull=False)
         print("Triggering Allele Info validation, this may take a while")
-        variant_matching_dry_run(ImportedAlleleInfo.objects.all())
+        variant_matching_dry_run(qs)
         print("Complete")
