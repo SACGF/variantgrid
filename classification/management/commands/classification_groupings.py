109ca563464efef2c10f272420630614e6a3f6ed
TheMadBug <jimmy.andrews@gmail.com>
2025-03-05 15:39:58 +1100

    More logging to show signs of life in ClassificationGrouping command line

--- classification/management/commands/classification_groupings.py
+++ classification/management/commands/classification_groupings.py
@@ -16,14 +16,19 @@ class Command(BaseCommand):
             raise ValueError("Must provide one or more of summary, all, dirty")
 
         if options.get("summary"):
-            for cm in ClassificationModification.objects.filter(is_last_published=True).select_related("classification").iterator():
+            for index, cm in enumerate(ClassificationModification.objects.filter(is_last_published=True).select_related("classification").iterator()):
                 classification = cm.classification
                 classification.summary = ClassificationSummaryCalculator(cm).cache_dict()
                 classification.save(update_fields=["summary"])
+                if index % 1000 == 0 and index:
+                    print(f"Updating {index} classification summaries")
 
         if options.get("all"):
-            for classification in Classification.objects.iterator():
+            for index, classification in enumerate(Classification.objects.iterator()):
                 ClassificationGrouping.assign_grouping_for_classification(classification)
+                if index % 1000 == 0 and index:
+                    print(f"Updating {index} classification assigned to classification groups")
+            print("About to update all classification groups")
             ClassificationGrouping.objects.all().update(dirty=True)
 
         if options.get("all") or options.get("dirty"):
