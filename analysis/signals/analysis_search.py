77859d4776b767cadffae7eaf768134f7f69aad1
Dave Lawrence <davmlaw@gmail.com>
2025-07-07 15:42:13 +0930

    SACGF/variantgrid#1133 -Analysis and Allele search shortcut (a101) are the same

--- analysis/signals/analysis_search.py
+++ analysis/signals/analysis_search.py
@@ -3,15 +3,15 @@ import re
 from analysis.models import Analysis
 from snpdb.search import search_receiver, SearchInputInstance, SearchExample
 
-ANALYSIS_PREFIX_PATTERN = re.compile(r"^a(\d+)$")
+ANALYSIS_PREFIX_PATTERN = re.compile(r"^an(\d+)$")
 
 
 @search_receiver(
     search_type=Analysis,
     pattern=ANALYSIS_PREFIX_PATTERN,
     example=SearchExample(
-        note="\"a\" followed by the analysis ID",
-        examples=["a1105"]
+        note="\"an\" followed by the analysis ID",
+        examples=["an1105"]
     )
 )
 def search_analysis(search_input: SearchInputInstance):
