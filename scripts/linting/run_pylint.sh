1d7739663902c3bd65bb658297d90dbd7d49b9bf
Dave Lawrence <davmlaw@gmail.com>
2025-07-09 17:31:15 +0930

    linting - config

--- scripts/linting/run_pylint.sh
+++ scripts/linting/run_pylint.sh
@@ -1,3 +1,4 @@
 export VARIANTGRID_DIR=$(dirname $0)/../..
+export DJANGO_SETTINGS_MODULE=variantgrid.settings
 
-pylint --rcfile=${VARIANTGRID_DIR}/config/pylint3.rc analysis annotation classification eventlog flags genes library manual ontology pathtests patients pedigree scripts seqauto snpdb sync uicore upload variantgrid variantopedia > lint.txt
+pylint --load-plugins pylint_django --rcfile=${VARIANTGRID_DIR}/config/pylint3.rc analysis annotation classification eventlog flags genes library manual ontology pathtests patients pedigree scripts seqauto snpdb sync uicore upload variantgrid variantopedia > lint.txt
