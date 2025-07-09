ae9a8571289564be0cca792d909868fe1d69c5a3
Dave Lawrence <davmlaw@gmail.com>
2025-07-09 16:38:35 +0930

    linting - config - remove app that's not there

--- scripts/linting/run_pylint.sh
+++ scripts/linting/run_pylint.sh
@@ -1,3 +1,3 @@
 export VARIANTGRID_DIR=$(dirname $0)/../..
 
-pylint --rcfile=${VARIANTGRID_DIR}/config/pylint3.rc analysis annotation auth classification eventlog flags genes library manual ontology pathtests patients pedigree scripts seqauto snpdb sync uicore upload variantgrid variantopedia > lint.txt
+pylint --rcfile=${VARIANTGRID_DIR}/config/pylint3.rc analysis annotation classification eventlog flags genes library manual ontology pathtests patients pedigree scripts seqauto snpdb sync uicore upload variantgrid variantopedia > lint.txt
