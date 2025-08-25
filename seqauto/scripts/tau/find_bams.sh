5fb501ab1ddb926ae9550c765827c17a89b3b13e
Dave Lawrence <davmlaw@gmail.com>
2025-08-25 15:18:48 +0930

    SACGF/variantgrid#351 - SeqAuto scan performance

--- seqauto/scripts/tau/find_bams.sh
+++ seqauto/scripts/tau/find_bams.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-find /tau/data/clinical_hg38 -name "*.bam"
+# BAMs are located eg:
+# /tau/data/clinical_hg38/idt_exome/Exome_21_110_211126_NB501008_0158_AH3LJHBGXL/1_BAM/26_GU_TRIO_PID_AFF_AE_2131009254B_recal_reads.hg38.bam
+# So only need to go in 4
+find /tau/data/clinical_hg38 -maxdepth 4 -name "*.bam"
 
