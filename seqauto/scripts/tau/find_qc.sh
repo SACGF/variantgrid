77081c3cb28d3e4ab2208100720742f980d00593
Dave Lawrence <davmlaw@gmail.com>
2025-08-25 20:02:57 +0930

    SACGF/variantgrid#351 - SeqAuto scan performance

--- seqauto/scripts/tau/find_qc.sh
+++ seqauto/scripts/tau/find_qc.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # This needs to return all QC objects
-TAU_DATA_DIR=/tau/data/clinical_hg38
 
-find ${TAU_DATA_DIR} -name "*_stats.txt" -o -name "*_qc_summary.txt" -o -name "*.per_gene_coverage.tsv.gz" -o  -path "*/0_goi/*.txt"
+# We want to be able to skip entire directories with .variantgrid_skip_flowcell in them - ie not descend any further
+find /tau/data/clinical_hg38 -mindepth 1 -maxdepth 1 -type d ! -exec test -e '{}/.variantgrid_skip_flowcell' \; -print0 \
+| xargs -0 -I{} find '{}' -name "*_stats.txt" -o -name "*_qc_summary.txt" -o -name "*.per_gene_coverage.tsv.gz" -o  -path "*/0_goi/*.txt"
--- seqauto/scripts/tau/find_vcfs.sh
+++ seqauto/scripts/tau/find_vcfs.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-find /tau/data/clinical_hg38 \( -name \*.vcf -o -name \*.vcf.gz \)
+# We want to be able to skip entire directories with .variantgrid_skip_flowcell in them - ie not descend any further
+find /tau/data/clinical_hg38 -mindepth 1 -maxdepth 1 -type d ! -exec test -e '{}/.variantgrid_skip_flowcell' \; -print0 \
+| xargs -0 -I{} find '{}' \( -name \*.vcf -o -name \*.vcf.gz \)
