ee062628606c92561e6c59bab8becf014cfaf98e
Dave Lawrence <davmlaw@gmail.com>
2025-08-28 14:30:40 +0930

    SACGF/variantgrid#351 - SeqAuto scan performance - typo in script

--- seqauto/scripts/tau/find_qc.sh
+++ seqauto/scripts/tau/find_qc.sh
@@ -5,5 +5,5 @@ FLOWCELL_DIRS=${OUTPUT_DIR}/find_flowcells.txt
 
 while read -r line
 do
-	ls -d -1 ${line}/4_QC/exec_stats/*qc_summary.txt ${line}/4_QC/bam_stats/samples/*.per_gene_coverage.tsv.gz ${lines}/0_goi/*.txt 2> /dev/null || true
+	ls -d -1 ${line}/4_QC/exec_stats/*qc_summary.txt ${line}/4_QC/bam_stats/samples/*.per_gene_coverage.tsv.gz ${line}/0_goi/*.txt 2> /dev/null || true
 done < "${FLOWCELL_DIRS}"
