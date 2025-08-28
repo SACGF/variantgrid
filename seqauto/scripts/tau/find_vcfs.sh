43206ca517f1a9287ee325f14b38de1d8cb48488
Dave Lawrence <davmlaw@gmail.com>
2025-08-28 13:40:48 +0930

    SACGF/variantgrid#351 - SeqAuto scan performance - also get combo VCFs

--- seqauto/scripts/tau/find_vcfs.sh
+++ seqauto/scripts/tau/find_vcfs.sh
@@ -8,5 +8,5 @@ FLOWCELL_DIRS=${OUTPUT_DIR}/find_flowcells.txt
 
 while read -r line
 do
-	ls -d -1 ${line}/2_variants/**/*.vcf ${line}/2_variants/**/*.vcf.gz 2> /dev/null || true
+	ls -d -1 ${line}/2_variants/*.vcf.gz ${line}/2_variants/**/*.vcf ${line}/2_variants/**/*.vcf.gz 2> /dev/null || true
 done < "${FLOWCELL_DIRS}"
