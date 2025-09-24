6aa2498171b11630a662acbad3fb2de901cffa52
Dave Lawrence <davmlaw@gmail.com>
2025-09-24 14:15:40 +0930

    SACGF/variantgrid_sapath#351 - seqauto scan performance patch back to master

--- seqauto/scripts/tau/find_flowcells.sh
+++ seqauto/scripts/tau/find_flowcells.sh
@@ -2,5 +2,8 @@
 
 # Only look in idt_* dirs - as that's where new stuff is put
 # RTAComplete.txt is in 4_QC/sequencing_stats so need to drop down 2 dirs (with dirname)
-find -L /tau/data/clinical_hg38 -maxdepth 5 -name "RTAComplete.txt" -exec dirname {} \; | xargs dirname | xargs dirname
+# find /tau/data/clinical_hg38 -maxdepth 5 -name "RTAComplete.txt" -exec dirname {} \; | xargs dirname | xargs dirname
+
+# We want to be able to skip entire directories with .variantgrid_skip_flowcell in them - ie not descend any further
+find /tau/data/clinical_hg38 -mindepth 1 -maxdepth 1 -type d ! -exec test -e '{}/.variantgrid_skip_flowcell' \; -print0 | xargs -0 -I{} find '{}' -maxdepth 4 -name 'RTAComplete.txt' | xargs -n1 dirname | xargs -n1 dirname | xargs -n1 dirname
 
