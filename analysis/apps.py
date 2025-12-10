4a4296dd0ba21c3b4c8f1dad90687970bcdb13ee
Dave Lawrence <davmlaw@gmail.com>
2025-12-10 15:29:47 +1030

    SACGF/variantgrid_sapath#373 - Auto run analyses for onco - forgot to hookup

--- analysis/apps.py
+++ analysis/apps.py
@@ -12,10 +12,11 @@ class AnalysisConfig(AppConfig):
         from analysis.signals import analysis_search, analysis_health_check
 
         from analysis.models import VariantTag
-        from analysis.signals.signal_handlers import variant_tag_create, variant_tag_delete
+        from analysis.signals.signal_handlers import variant_tag_create, variant_tag_delete, vcf_import_success
+        from snpdb.models import VCF
         from upload.signals.signals import vcf_import_success_signal
         # pylint: enable=import-outside-toplevel,unused-import
 
         post_save.connect(variant_tag_create, sender=VariantTag)
         post_delete.connect(variant_tag_delete, sender=VariantTag)
-        vcf_import_success_signal.connect()
+        vcf_import_success_signal.connect(vcf_import_success, sender=VCF)
