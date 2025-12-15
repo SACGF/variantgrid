a4a1b9a620eca1f1e3f0cf5f3ed00034ce4ac71c
Dave Lawrence <davmlaw@gmail.com>
2025-12-15 13:44:02 +1030

    SACGF/variantgrid_sapath#373 - Hook up signals properly

--- analysis/apps.py
+++ analysis/apps.py
@@ -12,11 +12,10 @@ class AnalysisConfig(AppConfig):
         from analysis.signals import analysis_search, analysis_health_check
 
         from analysis.models import VariantTag
-        from analysis.signals.signal_handlers import variant_tag_create, variant_tag_delete, vcf_import_success
-        from snpdb.models import VCF
+        from analysis.signals.signal_handlers import variant_tag_create, variant_tag_delete, handle_vcf_import_success
         from upload.signals.signals import vcf_import_success_signal
         # pylint: enable=import-outside-toplevel,unused-import
 
         post_save.connect(variant_tag_create, sender=VariantTag)
         post_delete.connect(variant_tag_delete, sender=VariantTag)
-        vcf_import_success_signal.connect(vcf_import_success)
+        vcf_import_success_signal.connect(handle_vcf_import_success)
