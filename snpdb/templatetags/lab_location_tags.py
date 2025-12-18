350f7d374b8ec85232b1f64d4e85adb6cc06227f
Dave Lawrence <davmlaw@gmail.com>
2025-12-19 09:44:05 +1030

    Make djgeojson and leaflet optional #1410

--- snpdb/templatetags/lab_location_tags.py
+++ snpdb/templatetags/lab_location_tags.py
@@ -6,31 +6,45 @@ from variantgrid import settings
 register = Library()
 
 
-@register.inclusion_tag("snpdb/tags/lab_locations.html")
+from django import template
+from django.template.loader import render_to_string
+from django.conf import settings
+
+register = template.Library()
+
+
+@register.simple_tag
 def lab_locations(labs=None, samples_only=False, involved_only=True,
                   center_lat=19.434403, center_long=37.238392, zoom_level=1):
+    # Made this a simple tag, so that if Leaflet is not loaded, we don't load any Leaflet tags
 
-    context = {}
-    if "leaflet" in settings.INSTALLED_APPS:
-        if labs is None:
-            labs = Lab.objects.all()
-        elif isinstance(labs, list):
-            labs = Lab.objects.filter(pk__in=[lab.pk for lab in labs])
+    if "leaflet" not in settings.INSTALLED_APPS:
+        return ""
 
-        labs = labs.filter(lat__isnull=False, long__isnull=False)
-        if samples_only:
-            labs = labs.filter(labproject__samplelabproject__sample__isnull=False)
+    if labs is None:
+        labs = Lab.objects.all()
+    elif isinstance(labs, list):
+        labs = Lab.objects.filter(pk__in=[lab.pk for lab in labs])
 
-        if involved_only:
-            labs = labs.filter(labproject__involved=True)
+    labs = labs.filter(lat__isnull=False, long__isnull=False)
 
-        context = {
+    if samples_only:
+        labs = labs.filter(labproject__samplelabproject__sample__isnull=False)
+    if involved_only:
+        labs = labs.filter(labproject__involved=True)
+
+    if not labs.exists():
+        return ""
+
+    return render_to_string(
+        "snpdb/tags/lab_locations.html",
+        {
             "labs": labs,
             "center_lat": center_lat,
             "center_long": center_long,
-            "zoom_level": zoom_level
+            "zoom_level": zoom_level,
         }
-    return context
+    )
 
 
 @register.inclusion_tag("snpdb/tags/lab_families.html")
