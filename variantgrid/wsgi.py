8bc3990c8bdaaa08832a28c161ffbfdf5d3a865f
Dave Lawrence <davmlaw@gmail.com>
2026-01-13 11:41:43 +1030

    Matplotlib headless mode #1411

--- variantgrid/wsgi.py
+++ variantgrid/wsgi.py
@@ -24,6 +24,7 @@ from django.dispatch import receiver
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "variantgrid.settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "variantgrid.settings")
+os.environ.setdefault("MPLBACKEND", "Agg")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
