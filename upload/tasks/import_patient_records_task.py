9555051daaf16385ba8035bc4ef813108db2e93f
Dave Lawrence <davmlaw@gmail.com>
2026-03-11 11:28:03 +1030

    Attempt to fix https://github.com/SACGF/variantgrid/security/code-scanning/130

--- upload/tasks/import_patient_records_task.py
+++ upload/tasks/import_patient_records_task.py
@@ -11,7 +11,6 @@ class ImportPatientRecords(ImportTask):
     def process_items(self, uploaded_file):
         patient_import = PatientImport.objects.create()
         patient_records = PatientRecords.objects.create(patient_import=patient_import)
-        logging.info("Created patient_records: %s", patient_records)
         UploadedPatientRecords.objects.create(uploaded_file=uploaded_file,
                                               patient_records=patient_records)
 
