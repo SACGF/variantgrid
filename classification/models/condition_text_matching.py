c152f4ca2814365b7aaec8f71657cfad5bfe327a
TheMadBug <jimmy.andrews@gmail.com>
2026-04-21 13:36:10 +1000

    Fix condition matching unexpected "terms"

--- classification/models/condition_text_matching.py
+++ classification/models/condition_text_matching.py
@@ -20,7 +20,7 @@ from model_utils.models import TimeStampedModel
 from annotation.regexes import db_ref_regexes
 from classification.enums import SpecialEKeys, ShareLevel
 from classification.models import Classification, ClassificationModification, classification_post_publish_signal, \
-    flag_types, EvidenceKeyMap, ConditionResolvedDict, ConditionResolved, classification_flag_types
+    flag_types, EvidenceKeyMap, ConditionResolvedDict, ConditionResolved, classification_flag_types, ConditionReference
 from classification.models.condition_text_search import condition_text_search
 from flags.models import flag_comment_action, Flag, FlagComment, FlagResolution
 from genes.models import GeneSymbol, GeneSymbolAlias
@@ -436,7 +436,10 @@ class ConditionTextMatch(TimeStampedModel, GuardianPermissionsMixin):
         """
         if terms := self.condition_xref_terms:
 
-            condition_resolved_obj = ConditionResolved(terms=_sort_terms(terms), join=None if len(terms) <= 1 else MultiCondition(self.condition_multi_operation))
+            condition_resolved_obj = ConditionResolved(
+                references=[ConditionReference(term) for term in _sort_terms(terms)],
+                join=None if len(terms) <= 1 else MultiCondition(self.condition_multi_operation)
+            )
             return condition_resolved_obj.to_json()
 
         return None
