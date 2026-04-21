b90a20dc5a2e63b4cba887300c3ad9aec51efe07
TheMadBug <jimmy.andrews@gmail.com>
2026-04-21 13:16:22 +1000

    Introduce custom django fields allowing for easy ORM to actual objects

--- library/utils/database_utils.py
+++ library/utils/database_utils.py
@@ -1,9 +1,11 @@
-from typing import Iterable, Optional, Any
+from typing import Iterable, Optional, Any, TypeVar, Generic, Type
 
 import sqlparse
-from django.db import connection, transaction
+from dataclasses_json import DataClassJsonMixin
+from django.db import models, connection, transaction
 # 970: Added transaction wrapper due to Postgres hanging query
 from django.db.models import QuerySet
+from django.db.models.enums import TextChoices, IntegerChoices
 
 from library.cache import timed_cache
 from library.constants import DAY_SECS
@@ -141,3 +143,115 @@ def sql_delete_qs(qs, batch_size: Optional[int] = None) -> int:
 
 def postgres_arrays(array):
     return "{%s}" % ','.join([str(s) if s is not None else "NULL" for s in array])
+
+
+T1 = TypeVar('T1', bound=DataClassJsonMixin)
+
+
+class JSONDataclassField(models.JSONField, Generic[T1]):
+    """
+    A field type that will return and expect a DataClassJsonMixin type.
+    e.g.
+    @dataclass
+    class Coordinate(DataClassJsonMixin):
+        x: int
+        y: int
+    Will be serialized into the database, and referring to this field will deserialize so you don't have to convert
+    from JSONB into your dataclass objects in custom code, it'll just be handled in ORM
+
+    PyCharm isn't smart enough to work out the execpted type, so just add the type hint after declaring using this in a model
+    """
+
+    def __init__(self,
+                 dataclass_type: Type[T1],
+                 illegal_value_result: Any = None,
+                 *args,
+                 **kwargs):
+        """
+        :param dataclass_type: The class to convert to/from JSON, should be @dataclass_json or DataClassJsonMixin
+        :param illegal_value_result: If we can't convert the JSONB from the database into dataclass_type, return this instead
+        """
+        self.dataclass_type = dataclass_type
+        self.illegal_value_result = illegal_value_result
+        super().__init__(*args, **kwargs)
+
+    @property
+    def non_db_attrs(self):
+        return super().non_db_attrs + ("dataclass_type", "illegal_value_result",)
+
+    def deconstruct(self):
+        name, path, args, kwargs = super().deconstruct()
+        kwargs["dataclass_type"] = self.dataclass_type
+        kwargs["illegal_value_result"] = self.illegal_value_result
+        return name, path, args, kwargs
+
+    def from_db_value(self, value, expression, connection) -> Optional[T1]:
+        if json_obj := super().from_db_value(value, expression, connection):
+            try:
+                return self.dataclass_type.from_dict(json_obj)
+            except ValueError as ve:
+                # TODO raise warning
+                print(f"Found illegal value in database {ve}")
+                return self.illegal_value_result
+        return None
+
+    def get_prep_value(self, value: Optional[T1]):
+        if hasattr(value, 'to_dict'):
+            return value.to_dict()
+        return value
+
+
+TX = TypeVar('TX')
+
+
+class ChoicesMixin(Generic[TX]):
+    """
+    While IntegerField, TextField and CharField all allow choices, they'll still return an integer or string.
+    TextChoices or IntegerChoices allow you to add additional methods, so using one of the concrete implementations
+    below will gice you an instance of the actual Choices instead of the raw string or integer value.
+
+    Lastly you just need to provide the type and if you don't provide choices, they'll be taken from the type
+
+    PyCharm isn't smart enough to work out the execpted type, so just add the type hint after declaring using this in a model
+    """
+
+    def __init__(self, choices_type: Type[TX], *args, **kwargs):
+        if "choices" not in kwargs:
+            kwargs["choices"] = choices_type.choices
+        self.choices_type = choices_type
+        super().__init__(*args, **kwargs)
+
+    def deconstruct(self):
+        name, path, args, kwargs = super().deconstruct()
+        kwargs["choices_type"] = self.choices_type
+        return name, path, args, kwargs
+
+    @property
+    def non_db_attrs(self):
+        return super().non_db_attrs + ("choices_type",)
+
+    def from_db_value(self, value, expression, connection) -> Optional[TX]:
+        if value:
+            return self.choices_type(value)
+        return None
+
+
+T2 = TypeVar('T2', bound=TextChoices)
+
+
+class TextFieldChoices(ChoicesMixin[T2], models.TextField):
+    pass
+
+
+T3 = TypeVar('T3', bound=TextChoices)
+
+
+class CharFieldChoices(ChoicesMixin[T3], models.CharField):
+    pass
+
+
+T4 = TypeVar('T4', bound=IntegerChoices)
+
+
+class IntegerFieldChoices(ChoicesMixin[T4], models.IntegerField):
+    pass
