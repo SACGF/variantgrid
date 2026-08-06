09b7cc7b80a97647e5d2d55e34991c1c95e89a4f
Dave Lawrence <davmlaw@gmail.com>
2026-08-06 21:17:17 +0930

    These have been superseeded by download_annotation_data management command

--- annotation/annotation_data/vep_download_37_and_38.sh
+++ annotation/annotation_data/vep_download_37_and_38.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Public data downloaded straight from source. Everything else (plugin/custom data files) comes
+# from "python3 manage.py download_annotation_data", which works the file list out from settings.
+
 BASE_DIR=$(dirname ${BASH_SOURCE[0]})
 ANNOTATION_DIR=/data/annotation
 
@@ -9,7 +12,5 @@ cd ${ANNOTATION_DIR}
 ${BASE_DIR}/vep_all_builds_download.sh
 
 ${BASE_DIR}/vep_grch37_data_download.sh
-${BASE_DIR}/vep_grch37_data_download_generated.sh
 
 ${BASE_DIR}/vep_grch38_data_download.sh
-${BASE_DIR}/vep_grch38_data_download_generated.sh
