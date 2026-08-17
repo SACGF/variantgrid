35fc008da723b3d78bec90083d86c5981e34089a
TheMadBug <jimmy.andrews@gmail.com>
2026-08-17 15:00:20 +1000

    Revert "Fix migration scripts that refer to refactored code"
    This reverts commit dd6b58a4d17f0a1fad478e13b9221b07e04462d5.

--- classification/migrations/0030_clinvar_mappings.py
+++ classification/migrations/0030_clinvar_mappings.py
@@ -3,7 +3,6 @@
 from django.db import migrations
 
 from classification.autopopulate_evidence_keys.clinvar_option_updator import ClinVarOptionUpdator
-from classification.evidence_key_rename import OptionUpdator
 
 
 def clinical_significance(apps, _schema_editor):
@@ -37,7 +36,7 @@ imprinting : Imprinting
 
 
 def mode_of_inheritance(apps, _schema_editor):
-    options = EvidenceKeyOptionUpdator(apps, "mode_of_inheritance")
+    options = ClinVarOptionUpdator(apps, "mode_of_inheritance")
     options.set_clinvar_option("autosomal_dominant", "Autosomal dominant inheritance")
     options.set_clinvar_option("autosomal_recessive", "Autosomal recessive inheritance")
     options.set_clinvar_option("autosomal_unknown", "Autosomal unknown")
@@ -59,7 +58,7 @@ def mode_of_inheritance(apps, _schema_editor):
 
 
 def allele_origin(apps, _schema_editor):
-    options = EvidenceKeyOptionUpdator(apps, "allele_origin")
+    options = ClinVarOptionUpdator(apps, "allele_origin")
     options.set_clinvar_option("germline", "germline")
     options.set_clinvar_option("somatic", "somatic")
     options.set_clinvar_option("unknown", "unknown")
