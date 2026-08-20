26e42223a52f43bd6de7c1afb33c1c677fb79309
Dave Lawrence <davmlaw@gmail.com>
2026-08-20 15:10:53 +0930

    Fix import syntax cut off - must have been merge error

--- seqauto/tasks/gold_summary_tasks.py
+++ seqauto/tasks/gold_summary_tasks.py
@@ -23,6 +23,12 @@ from library.enums.log_level import LogLevel
 from library.log_utils import get_traceback, log_traceback
 from library.utils import get_single_element
 from seqauto.models import (
+    EnrichmentKit,
+    GoldCoverageSummary,
+    GoldGeneCoverageCollection,
+    GoldReference,
+    SequencingRun,
+)
 from snpdb.models.models_enums import ImportStatus
 
 
