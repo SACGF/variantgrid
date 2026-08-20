2bc6387fa47ae043d7318ef94a5017de112c15b6
Dave Lawrence <davmlaw@gmail.com>
2026-08-20 10:22:08 +0930

    comment

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -5,8 +5,7 @@ set -e
 VG_DIR=$(dirname ${BASH_SOURCE[0]})
 export PYTHONPATH=${PYTHONPATH}:${VG_DIR};
 
-# The migrator's first step calls manage.py, which imports settings -> INSTALLED_APPS -> every 3rd
-# party package, so the current requirements have to be installed before it starts
+# Install now, in case user did manual git pull and it will fail running migrator
 "${VG_DIR}/install_requirements.sh"
 
 python3 "./scripts/migrator/migrator.py" $1
