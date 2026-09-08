9e59af8aa7069cd43f4fa0dd9e8bd7f7f6b4fd3a
Dave Lawrence <davmlaw@gmail.com>
2026-09-08 11:51:20 +0930

    Check for somalier VCF

--- variantgrid/deployment_validation/somalier_check.py
+++ variantgrid/deployment_validation/somalier_check.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 from subprocess import check_output
 from typing import Optional
@@ -28,13 +29,20 @@ def check_somalier() -> dict:
     }
     somalier_cfg = SomalierConfig()
     for genome_build in GenomeBuild.builds_with_annotation():
+        sites_path = somalier_cfg.get_sites(genome_build)
+        somalier_data[f"somalier_sites_file_{genome_build.name}"] = {
+            "valid": os.path.exists(sites_path),
+            "fix": f"Download somalier sites VCF to {sites_path} - see scripts/install/get_somalier_release.sh",
+        }
+
         try:
             somalier_cfg.get_sites_vcf(genome_build)
             valid = True
         except Exception:
             valid = False
+        sites_vcf_name = somalier_cfg.get_sites_vcf_name(genome_build)
         somalier_data[f"somalier_sites_vcf_{genome_build.name}"] = {
             "valid": valid,
-            "fix": somalier_cfg.get_sites_vcf_name(genome_build),
+            "fix": f"Upload/import '{sites_vcf_name}' as a VCF for {genome_build} (the VCF name must match the sites filename)",
         }
     return somalier_data
