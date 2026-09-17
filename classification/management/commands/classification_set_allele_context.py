8a212d5f6f5c2f27e0441d5eefd7f29e16e13b46
Dave Lawrence <davmlaw@gmail.com>
2026-09-17 15:10:31 +0930

    classification_set_allele_context: add master's maintenance category (#1816)

--- classification/management/commands/classification_set_allele_context.py
+++ classification/management/commands/classification_set_allele_context.py
@@ -3,6 +3,8 @@ from django.core.management import BaseCommand
 
 class Command(BaseCommand):
 
+    category = "maintenance"
+
     def handle(self, *args, **options):
         # No longer relevant
         pass
