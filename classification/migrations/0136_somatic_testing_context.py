c1a2e445bea2e5ab21256ee105a651b3c579c085
TheMadBug <jimmy.andrews@gmail.com>
2026-09-23 14:16:21 +1000

    Undo accidental refactor of evidence key names in migraiton scripts

--- classification/migrations/0136_somatic_testing_context.py
+++ classification/migrations/0136_somatic_testing_context.py
@@ -25,7 +25,7 @@ def _new_ekey_testing_context(apps, _schema_editor):
 
 def _delete_new_ekey(apps, _schema_editor):
     EvidenceKey = apps.get_model("classification", "EvidenceKey")
-    EvidenceKey.objects.filter(key="somatic:testing_context_bucket").delete()
+    EvidenceKey.objects.filter(key="somatic:testing_context").delete()
 
 
 class Migration(migrations.Migration):
